33b9ff97cd9733edaa7f1989073de5426629dead
smolder on v.nix.is <smolder@v.nix.is>
2010-08-05 16:00:05 +0000

    Bumping smolder reports for Git

---
+++ 3/t3402-rebase-merge.sh
@@ -0,0 +1,10 @@
+ok 1 - setup
+ok 2 - reference merge
+ok 3 - rebase
+ok 4 - test-rebase@{1} is pre rebase
+ok 5 - merge and rebase should match
+ok 6 - rebase the other way
+ok 7 - merge and rebase should match
+ok 8 - picking rebase
+# passed all 8 test(s)
+1..8
---
+++ 3/t4202-log.sh
@@ -0,0 +1,29 @@
+ok 1 - setup
+ok 2 - pretty
+ok 3 - pretty (tformat)
+ok 4 - pretty (shortcut)
+ok 5 - format
+ok 6 - format %w(12,1,2)
+ok 7 - format %w(,1,2)
+ok 8 - oneline
+ok 9 - diff-filter=A
+ok 10 - diff-filter=M
+ok 11 - diff-filter=D
+ok 12 - diff-filter=R
+ok 13 - diff-filter=C
+ok 14 - git log --follow
+ok 15 - git log --no-walk <commits> sorts by commit time
+ok 16 - git show <commits> leaves list of commits as given
+ok 17 - setup case sensitivity tests
+ok 18 - log --grep
+ok 19 - log -i --grep
+ok 20 - log --grep -i
+ok 21 - simple log --graph
+ok 22 - set up merge history
+ok 23 - log --graph with merge
+ok 24 - log --graph with full output
+ok 25 - set up more tangled history
+ok 26 - log --graph with merge
+ok 27 - log.decorate configuration
+# passed all 27 test(s)
+1..27
