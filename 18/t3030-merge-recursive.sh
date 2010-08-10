e5e7e1983a902d1c794ebdde8c83933bbfa9c702
smolder on v.nix.is <smolder@v.nix.is>
2010-08-10 20:00:48 +0000

    Bumping smolder reports for Git

---
+++ 18/t3030-merge-recursive.sh
@@ -0,0 +1,30 @@
+ok 1 - setup 1
+ok 2 - setup 2
+ok 3 - setup 3
+ok 4 - setup 4
+ok 5 - setup 5
+ok 6 - setup 6
+ok 7 - setup 7
+ok 8 - merge-recursive simple
+ok 9 - merge-recursive result
+ok 10 - fail if the index has unresolved entries
+ok 11 - merge-recursive remove conflict
+ok 12 - merge-recursive remove conflict
+ok 13 - merge-recursive d/f simple
+ok 14 - merge-recursive result
+ok 15 - merge-recursive d/f conflict
+ok 16 - merge-recursive d/f conflict result
+ok 17 - merge-recursive d/f conflict the other way
+ok 18 - merge-recursive d/f conflict result the other way
+ok 19 - merge-recursive d/f conflict
+ok 20 - merge-recursive d/f conflict result
+ok 21 - merge-recursive d/f conflict
+ok 22 - merge-recursive d/f conflict result
+ok 23 - reset and 3-way merge
+ok 24 - reset and bind merge
+ok 25 - merge removes empty directories
+not ok 26 - merge-recursive simple w/submodule # TODO known breakage
+not ok 27 - merge-recursive simple w/submodule result # TODO known breakage
+# still have 2 known breakage(s)
+# passed all remaining 25 test(s)
+1..27
---
+++ 18/t5600-clone-fail-cleanup.sh
@@ -0,0 +1,7 @@
+ok 1 - clone of non-existent source should fail
+ok 2 - failed clone should not leave a directory
+ok 3 - clone of non-existent (relative to $PWD) source should fail
+ok 4 - clone should work now that source exists
+ok 5 - successful clone must leave the directory
+# passed all 5 test(s)
+1..5
