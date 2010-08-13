64f6c930da837ad038844e5e8bc7ef9e206c771c
smolder on v.nix.is <smolder@v.nix.is>
2010-08-13 20:00:20 +0000

    Bumping smolder reports for Git

---
+++ 25/t1304-default-acl.sh
@@ -0,0 +1,5 @@
+ok 1 - Setup test repo
+ok 2 - Objects creation does not break ACLs with restrictive umask
+ok 3 - git gc does not break ACLs with restrictive umask
+# passed all 3 test(s)
+1..3
---
+++ 25/t3902-quoted.sh
@@ -0,0 +1,14 @@
+ok 1 - setup
+ok 2 - check fully quoted output from ls-files
+ok 3 - check fully quoted output from diff-files
+ok 4 - check fully quoted output from diff-index
+ok 5 - check fully quoted output from diff-tree
+ok 6 - check fully quoted output from ls-tree
+ok 7 - setting core.quotepath
+ok 8 - check fully quoted output from ls-files
+ok 9 - check fully quoted output from diff-files
+ok 10 - check fully quoted output from diff-index
+ok 11 - check fully quoted output from diff-tree
+ok 12 - check fully quoted output from ls-tree
+# passed all 12 test(s)
+1..12
