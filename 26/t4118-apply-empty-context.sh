bcab626577c2cd13b000e6d2d1e3ebefa03123b1
smolder on v.nix.is <smolder@v.nix.is>
2010-08-14 02:00:25 +0000

    Bumping smolder reports for Git

---
+++ 26/t4118-apply-empty-context.sh
@@ -0,0 +1,5 @@
+ok 1 - setup
+ok 2 - apply --numstat
+ok 3 - apply --apply
+# passed all 3 test(s)
+1..3
---
+++ 26/t6010-merge-base.sh
@@ -0,0 +1,12 @@
+ok 1 - compute merge-base (single)
+ok 2 - compute merge-base (all)
+ok 3 - compute merge-base with show-branch
+ok 4 - compute merge-base (single)
+ok 5 - compute merge-base (all)
+ok 6 - merge-base for octopus-step (setup)
+ok 7 - merge-base A B C
+ok 8 - merge-base A B C using show-branch
+ok 9 - criss-cross merge-base for octopus-step (setup)
+ok 10 - merge-base B A^^ A^^2
+# passed all 10 test(s)
+1..10
