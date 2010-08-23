8c765d5bc87ed3e28ea0d292b41ed81ca3610c64
smolder on v.nix.is <smolder@v.nix.is>
2010-08-23 02:00:42 +0000

    Bumping smolder reports for Git

---
+++ 43/t3040-subprojects-basic.sh
@@ -0,0 +1,13 @@
+ok 1 - Super project creation
+ok 2 - create subprojects
+ok 3 - check if fsck ignores the subprojects
+ok 4 - check if commit in a subproject detected
+ok 5 - check if a changed subproject HEAD can be committed
+ok 6 - check if diff-index works for subproject elements
+ok 7 - check if diff-tree works for subproject elements
+ok 8 - check if git diff works for subproject elements
+ok 9 - check if clone works
+ok 10 - removing and adding subproject
+ok 11 - checkout in superproject
+# passed all 11 test(s)
+1..11
---
+++ 43/t9112-git-svn-md5less-file.sh
@@ -0,0 +1,5 @@
+ok 1 - load svn dumpfile
+ok 2 - initialize git svn
+ok 3 - fetch revisions from svn
+# passed all 3 test(s)
+1..3
