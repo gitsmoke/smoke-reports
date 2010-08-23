2f624a4f49c440cfc8ce00d2ef35c0778be1bd72
smolder on v.nix.is <smolder@v.nix.is>
2010-08-23 12:00:28 +0000

    Bumping smolder reports for Git

---
+++ 44/t3407-rebase-abort.sh
@@ -0,0 +1,9 @@
+ok 1 - setup
+ok 2 - rebase --abort
+ok 3 - rebase --abort after --skip
+ok 4 - rebase --abort after --continue
+ok 5 - rebase --merge --abort
+ok 6 - rebase --merge --abort after --skip
+ok 7 - rebase --merge --abort after --continue
+# passed all 7 test(s)
+1..7
