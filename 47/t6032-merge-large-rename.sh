3f833366ab05ff73bb21f9b1ffd3186be139a841
smolder on v.nix.is <smolder@v.nix.is>
2010-08-26 02:00:54 +0000

    Bumping smolder reports for Git

---
+++ 47/t6032-merge-large-rename.sh
@@ -0,0 +1,10 @@
+ok 1 - setup (initial)
+ok 2 - rename (5, ok)
+ok 3 - set diff.renamelimit to 4
+ok 4 - rename (4, ok)
+ok 5 - rename (5, fail)
+ok 6 - set merge.renamelimit to 5
+ok 7 - rename (5, ok)
+ok 8 - rename (6, fail)
+# passed all 8 test(s)
+1..8
