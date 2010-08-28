53c1f160b2fb9969090629679e86c27cab839b16
smolder on v.nix.is <smolder@v.nix.is>
2010-08-28 02:01:03 +0000

    Bumping smolder reports for Git

---
+++ 49/t7609-merge-co-error-msgs.sh
@@ -0,0 +1,8 @@
+ok 1 - setup
+ok 2 - untracked files overwritten by merge
+ok 3 - untracked files or local changes ovewritten by merge
+ok 4 - cannot switch branches because of local changes
+ok 5 - not uptodate file porcelain checkout error
+ok 6 - not_uptodate_dir porcelain checkout error
+# passed all 6 test(s)
+1..6
