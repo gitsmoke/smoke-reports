7f766539cbdbea491aacda6c1f1227a0b7c97775
smolder on v.nix.is <smolder@v.nix.is>
2010-08-30 02:01:08 +0000

    Bumping smolder reports for Git

---
+++ 52/t4302-log-line-merge-history.sh
@@ -0,0 +1,12 @@
+ok 1 - Add path0 and commit.
+ok 2 - Change path0 in master.
+ok 3 - Make a new branch from the base commit
+ok 4 - Change path0 in feature.
+ok 5 - Merge the master to feature
+ok 6 - Resolve the conflict
+ok 7 - Show the line level log of path0
+ok 8 - Show the line log of the 2 line of path0 with graph
+ok 9 - validate the output.
+ok 10 - validate the graph output.
+# passed all 10 test(s)
+1..10
