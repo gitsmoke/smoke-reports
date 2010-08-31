4a507aa4ec5f539349323dbb368cd0b9be9095fe
smolder on v.nix.is <smolder@v.nix.is>
2010-08-31 02:01:23 +0000

    Bumping smolder reports for Git

---
+++ 53/t5510-fetch.sh
@@ -0,0 +1,34 @@
+ok 1 - setup
+ok 2 - clone and setup child repos
+ok 3 - fetch test
+ok 4 - fetch test for-merge
+ok 5 - fetch tags when there is no tags
+ok 6 - fetch following tags
+ok 7 - fetch must not resolve short tag name
+ok 8 - fetch must not resolve short remote name
+ok 9 - create bundle 1
+ok 10 - header of bundle looks right
+ok 11 - create bundle 2
+ok 12 - unbundle 1
+ok 13 - bundle 1 has only 3 files 
+ok 14 - unbundle 2
+ok 15 - bundle does not prerequisite objects
+ok 16 - bundle should be able to create a full history
+ok 17 - fetch via rsync
+ok 18 - push via rsync
+ok 19 - push via rsync
+ok 20 - fetch with a non-applying branch.<name>.merge
+ok 21 - fetch from GIT URL with a non-applying branch.<name>.merge
+ok 22 - quoting of a strangely named repo
+ok 23 - bundle should record HEAD correctly
+ok 24 - explicit fetch should not update tracking
+ok 25 - explicit pull should not update tracking
+ok 26 - configured fetch updates tracking
+ok 27 - pushing nonexistent branch by mistake should not segv
+ok 28 - auto tag following fetches minimum
+ok 29 - refuse to fetch into the current branch
+ok 30 - fetch into the current branch with --update-head-ok
+ok 31 - fetch --dry-run
+ok 32 - should be able to fetch with duplicate refspecs
+# passed all 32 test(s)
+1..32
