8e1a4ab7d0113e0ed9f5534a7a80ebc2087744cf
smolder on v.nix.is <smolder@v.nix.is>
2010-09-01 02:01:26 +0000

    Bumping smolder reports for Git

---
+++ 54/t0202-gettext-perl.sh
@@ -0,0 +1,14 @@
+# lib-gettext: No GETTEXT support available
+# run 1: Perl Git::I18N API (/usr/bin/perl /tmp/build-and-install-git-2010-09-01-CImg/t/t0202/test.pl)
+1..9
+ok 1 - Testing Git::I18N version 0.01 with Locale::Messages version 1.20
+ok 2 - Git::I18N is located at /tmp/build-and-install-git-2010-09-01-CImg/t/../perl/blib/lib/Git/I18N.pm
+ok 3 - sanity: Git::I18N defines a $VERSION
+ok 4 - sanity: Git::I18N has 1 export(s)
+ok 5 - sanity: Git::I18N exports everything by default
+ok 6 - sanity: __ has a $ prototype
+ok 7 - Passing a string through __() in the C locale works
+ok 8 # skip GETTEXT_LOCALE must be set by lib-gettext.sh for exhaustive Git::I18N tests
+ok 9 # skip GETTEXT_LOCALE must be set by lib-gettext.sh for exhaustive Git::I18N tests
+# test_external test Perl Git::I18N API was ok
+# test_external_without_stderr test no stderr: Perl Git::I18N API was ok
