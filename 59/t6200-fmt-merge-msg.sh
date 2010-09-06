1b18f007fca26e57d09e2cb7b64ef4a41fd1cb65
smolder on v.nix.is <smolder@v.nix.is>
2010-09-06 02:01:29 +0000

    Bumping smolder reports for Git

---
+++ 59/t6200-fmt-merge-msg.sh
@@ -0,0 +1,18 @@
+ok 1 - setup
+ok 2 - message for merging local branch
+ok 3 - message for merging external branch
+ok 4 - [merge] summary/log configuration
+ok 5 - configurable shortlog length: merge.log
+ok 6 - configurable shortlog length: --log
+ok 7 - fmt-merge-msg -m
+ok 8 - setup: expected shortlog for two branches
+ok 9 - shortlog for two branches
+ok 10 - merge-msg -F
+ok 11 - merge-msg -F in subdirectory
+ok 12 - merge-msg with nothing to merge
+ok 13 - merge-msg tag
+ok 14 - merge-msg two tags
+ok 15 - merge-msg tag and branch
+ok 16 - merge-msg lots of commits
+# passed all 16 test(s)
+1..16
---
+++ 59/t9114-git-svn-dcommit-merge.sh
@@ -0,0 +1,8 @@
+ok 1 - setup svn repository
+ok 2 - setup git mirror and merge
+ok 3 - verify pre-merge ancestry
+ok 4 - git svn dcommit merges
+ok 5 - verify post-merge ancestry
+ok 6 - verify merge commit message
+# passed all 6 test(s)
+1..6
