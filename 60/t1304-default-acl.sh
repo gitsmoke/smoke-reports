8b5bc33f0feff35c52ea26825c4883921bf6f69a
smolder on v.nix.is <smolder@v.nix.is>
2010-09-07 02:01:21 +0000

    Bumping smolder reports for Git

---
+++ 60/t1304-default-acl.sh
@@ -0,0 +1,6 @@
+Unable to use setfacl (output: 'setfacl: .: Operation not supported'; return code: '1')
+ok 1 # skip Setup test repo (missing SETFACL)
+ok 2 # skip Objects creation does not break ACLs with restrictive umask (missing SETFACL)
+ok 3 # skip git gc does not break ACLs with restrictive umask (missing SETFACL)
+# passed all 3 test(s)
+1..3
