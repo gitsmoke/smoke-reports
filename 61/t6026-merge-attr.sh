f365a1b89e61e4db952eef634ca7bac826b9a973
smolder on v.nix.is <smolder@v.nix.is>
2010-09-08 02:01:35 +0000

    Bumping smolder reports for Git

---
+++ 61/t6026-merge-attr.sh
@@ -0,0 +1,10 @@
+ok 1 - setup
+ok 2 - merge
+ok 3 - check merge result in index
+ok 4 - check merge result in working tree
+ok 5 - retry the merge with longer context
+ok 6 - custom merge backend
+ok 7 - custom merge backend
+ok 8 - up-to-date merge without common ancestor
+# passed all 8 test(s)
+1..8
