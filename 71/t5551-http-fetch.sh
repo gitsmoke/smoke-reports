6c317b84d50faacebf9bcdac816e6848584f6966
smolder on v.nix.is <smolder@v.nix.is>
2010-09-12 02:01:38 +0000

    Bumping smolder reports for Git

---
+++ 71/t5551-http-fetch.sh
@@ -0,0 +1,7 @@
+ok 1 - setup repository
+ok 2 - create http-accessible bare repository
+ok 3 - clone http repository
+ok 4 - fetch changes via http
+ok 5 - used upload-pack service
+# passed all 5 test(s)
+1..5
---
+++ 72/t4040-whitespace-status.sh
@@ -0,0 +1,11 @@
+ok 1 - setup
+ok 2 - diff-tree --exit-code
+ok 3 - diff-tree -b --exit-code
+ok 4 - diff-index --cached --exit-code
+ok 5 - diff-index -b -p --cached --exit-code
+ok 6 - diff-index --exit-code
+ok 7 - diff-index -b -p --exit-code
+ok 8 - diff-files --exit-code
+ok 9 - diff-files -b -p --exit-code
+# passed all 9 test(s)
+1..9
