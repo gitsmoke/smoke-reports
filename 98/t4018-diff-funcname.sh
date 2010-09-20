491d56c6dce99726bf14f619fdde76ec4df6845b
smolder on v.nix.is <smolder@v.nix.is>
2010-09-20 04:04:51 +0000

    Bumping smolder reports for Git

---
+++ 98/t4018-diff-funcname.sh
@@ -0,0 +1,30 @@
+ok 1 - builtin bibtex pattern compiles
+ok 2 - builtin bibtex wordRegex pattern compiles
+ok 3 - builtin cpp pattern compiles
+ok 4 - builtin cpp wordRegex pattern compiles
+ok 5 - builtin csharp pattern compiles
+ok 6 - builtin csharp wordRegex pattern compiles
+ok 7 - builtin html pattern compiles
+ok 8 - builtin html wordRegex pattern compiles
+ok 9 - builtin java pattern compiles
+ok 10 - builtin java wordRegex pattern compiles
+ok 11 - builtin objc pattern compiles
+ok 12 - builtin objc wordRegex pattern compiles
+ok 13 - builtin pascal pattern compiles
+ok 14 - builtin pascal wordRegex pattern compiles
+ok 15 - builtin php pattern compiles
+ok 16 - builtin php wordRegex pattern compiles
+ok 17 - builtin python pattern compiles
+ok 18 - builtin python wordRegex pattern compiles
+ok 19 - builtin ruby pattern compiles
+ok 20 - builtin ruby wordRegex pattern compiles
+ok 21 - builtin tex pattern compiles
+ok 22 - builtin tex wordRegex pattern compiles
+ok 23 - default behaviour
+ok 24 - preset java pattern
+ok 25 - custom pattern
+ok 26 - last regexp must not be negated
+ok 27 - pattern which matches to end of line
+ok 28 - alternation in pattern
+# passed all 28 test(s)
+1..28
---
+++ 98/t6013-rev-list-reverse-parents.sh
@@ -0,0 +1,5 @@
+ok 1 - set up --reverse example
+ok 2 - --reverse --parents --full-history combines correctly
+ok 3 - --boundary does too
+# passed all 3 test(s)
+1..3
