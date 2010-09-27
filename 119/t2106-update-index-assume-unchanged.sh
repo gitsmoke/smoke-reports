aea91fe3c32cee45e1382599732f01f9239610aa
smolder on v.nix.is <smolder@v.nix.is>
2010-09-27 02:01:10 +0000

    Bumping smolder reports for Git

---
+++ 119/t2106-update-index-assume-unchanged.sh
@@ -0,0 +1,4 @@
+ok 1 - setup
+ok 2 - do not switch branches with dirty file
+# passed all 2 test(s)
+1..2
---
+++ 119/t6020-merge-df.sh
@@ -0,0 +1,5 @@
+ok 1 - prepare repository
+ok 2 - Merge with d/f conflicts
+ok 3 - F/D conflict
+# passed all 3 test(s)
+1..3
