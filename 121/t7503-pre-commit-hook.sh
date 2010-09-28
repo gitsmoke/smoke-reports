ca10c60830e63af72e2642b9e52ca8c4a0a9365e
smolder on v.nix.is <smolder@v.nix.is>
2010-09-28 02:01:15 +0000

    Bumping smolder reports for Git

---
+++ 121/t7503-pre-commit-hook.sh
@@ -0,0 +1,10 @@
+ok 1 - with no hook
+ok 2 - --no-verify with no hook
+ok 3 - with succeeding hook
+ok 4 - --no-verify with succeeding hook
+ok 5 - with failing hook
+ok 6 - --no-verify with failing hook
+ok 7 - with non-executable hook
+ok 8 - --no-verify with non-executable hook
+# passed all 8 test(s)
+1..8
