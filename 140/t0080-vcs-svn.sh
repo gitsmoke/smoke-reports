6e02273a62615ff89dbe563ec2483cc0de1509d6
smolder on v.nix.is <smolder@v.nix.is>
2010-10-05 02:02:58 +0000

    Bumping smolder reports for Git

---
+++ 140/t0080-vcs-svn.sh
@@ -0,0 +1,9 @@
+ok 1 - obj pool: store data
+ok 2 - obj pool: NULL is offset ~0
+ok 3 - obj pool: out-of-bounds access
+ok 4 - obj pool: high-water mark
+ok 5 - line buffer
+ok 6 - string pool
+ok 7 - treap sort
+# passed all 7 test(s)
+1..7
---
+++ 140/t6200-fmt-merge-msg.sh
@@ -0,0 +1,25 @@
+ok 1 - setup
+ok 2 - message for merging local branch
+ok 3 - message for merging external branch
+ok 4 - [merge] summary/log configuration
+ok 5 - setup: clear [merge] configuration
+ok 6 - setup FETCH_HEAD
+ok 7 - merge.log=3 limits shortlog length
+ok 8 - merge.log=5 shows all 5 commits
+ok 9 - merge.log=0 disables shortlog
+ok 10 - --log=3 limits shortlog length
+ok 11 - --log=5 shows all 5 commits
+ok 12 - --no-log disables shortlog
+ok 13 - --log=0 disables shortlog
+ok 14 - fmt-merge-msg -m
+ok 15 - setup: expected shortlog for two branches
+ok 16 - shortlog for two branches
+ok 17 - merge-msg -F
+ok 18 - merge-msg -F in subdirectory
+ok 19 - merge-msg with nothing to merge
+ok 20 - merge-msg tag
+ok 21 - merge-msg two tags
+ok 22 - merge-msg tag and branch
+ok 23 - merge-msg lots of commits
+# passed all 23 test(s)
+1..23
