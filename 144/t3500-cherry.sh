5723e8197d42e0caedbe19149e85c39785b1de6f
smolder on v.nix.is <smolder@v.nix.is>
2010-10-06 02:01:48 +0000

    Bumping smolder reports for Git

---
+++ 144/t3500-cherry.sh
@@ -0,0 +1,5 @@
+ok 1 - prepare repository with topic branch, and check cherry finds the 2 patches from there
+ok 2 - check that cherry with limit returns only the top patch
+ok 3 - cherry-pick one of the 2 patches, and check cherry recognized one and only one as new
+# passed all 3 test(s)
+1..3
---
+++ 145/t4027-diff-submodule.sh
@@ -0,0 +1,24 @@
+ok 1 - setup
+ok 2 - git diff --raw HEAD
+ok 3 - git diff-index --raw HEAD
+ok 4 - git diff-files --raw
+ok 5 - git diff HEAD
+ok 6 - git diff HEAD with dirty submodule (work tree)
+ok 7 - git diff HEAD with dirty submodule (index)
+ok 8 - git diff HEAD with dirty submodule (untracked)
+ok 9 - git diff HEAD with dirty submodule (work tree, refs match)
+ok 10 - git diff HEAD with dirty submodule (work tree, refs match) [.git/config]
+ok 11 - git diff HEAD with dirty submodule (work tree, refs match) [.gitmodules]
+ok 12 - git diff HEAD with dirty submodule (index, refs match)
+ok 13 - git diff HEAD with dirty submodule (untracked, refs match)
+ok 14 - git diff HEAD with dirty submodule (untracked, refs match) [.git/config]
+ok 15 - git diff HEAD with dirty submodule (untracked, refs match) [.gitmodules]
+ok 16 - git diff between submodule commits
+ok 17 - git diff between submodule commits [.git/config]
+ok 18 - git diff between submodule commits [.gitmodules]
+ok 19 - git diff (empty submodule dir)
+ok 20 - conflicted submodule setup
+ok 21 - combined (empty submodule)
+ok 22 - combined (with submodule)
+# passed all 22 test(s)
+1..22
