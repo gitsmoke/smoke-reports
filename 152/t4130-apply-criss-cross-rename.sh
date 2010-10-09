1a5f593175975fe7baff5e95f38ee4ca483a0a88
smolder on v.nix.is <smolder@v.nix.is>
2010-10-09 02:01:39 +0000

    Bumping smolder reports for Git

---
+++ 152/t4130-apply-criss-cross-rename.sh
@@ -0,0 +1,9 @@
+ok 1 - setup
+ok 2 - criss-cross rename
+ok 3 - diff -M -B
+ok 4 - apply
+ok 5 - criss-cross rename
+ok 6 - diff -M -B
+ok 7 - apply
+# passed all 7 test(s)
+1..7
---
+++ 152/t7060-wtstatus.sh
@@ -0,0 +1,7 @@
+ok 1 - setup
+ok 2 - A/A conflict
+ok 3 - Report path with conflict
+ok 4 - Report new path with conflict
+ok 5 - M/D conflict does not segfault
+# passed all 5 test(s)
+1..5
