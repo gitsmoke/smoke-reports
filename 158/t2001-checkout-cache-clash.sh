47db2472b36aaa5934a98cd2a01917a969492a78
smolder on v.nix.is <smolder@v.nix.is>
2010-10-11 02:01:50 +0000

    Bumping smolder reports for Git

---
+++ 158/t2001-checkout-cache-clash.sh
@@ -0,0 +1,11 @@
+ok 1 - git update-index --add path0/file0
+ok 2 - writing tree out with git write-tree
+ok 3 - git update-index --add path1/file1
+ok 4 - writing tree out with git write-tree
+ok 5 - read previously written tree and checkout.
+ok 6 - git update-index --add a symlink.
+ok 7 - writing tree out with git write-tree
+ok 8 - read previously written tree and checkout.
+ok 9 - checking out conflicting path with -f
+# passed all 9 test(s)
+1..9
---
+++ 158/t9126-git-svn-follow-deleted-readded-directory.sh
@@ -0,0 +1,4 @@
+ok 1 - load repository
+ok 2 - fetch repository
+# passed all 2 test(s)
+1..2
