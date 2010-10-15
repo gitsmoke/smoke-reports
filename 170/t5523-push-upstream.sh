b197d292fdc47d390e81247392e0215e05b8ce94
smolder on v.nix.is <smolder@v.nix.is>
2010-10-15 02:03:22 +0000

    Bumping smolder reports for Git

---
+++ 170/t5523-push-upstream.sh
@@ -0,0 +1,14 @@
+ok 1 - setup bare parent
+ok 2 - setup local commit
+ok 3 - push -u master:master
+ok 4 - push -u master:other
+ok 5 - push -u --dry-run master:otherX
+ok 6 - push -u master2:master2
+ok 7 - push -u master2:other2
+ok 8 - push -u :master2
+ok 9 - push -u --all
+ok 10 - push -u HEAD
+ok 11 - progress messages to non-tty
+ok 12 - progress messages to non-tty (forced)
+# passed all 12 test(s)
+1..12
---
+++ 172/t6018-rev-list-glob.sh
@@ -0,0 +1,32 @@
+ok 1 - setup
+ok 2 - rev-parse --glob=refs/heads/subspace/*
+ok 3 - rev-parse --glob=heads/subspace/*
+ok 4 - rev-parse --glob=refs/heads/subspace/
+ok 5 - rev-parse --glob=heads/subspace/
+ok 6 - rev-parse --glob=heads/subspace
+ok 7 - rev-parse --branches=subspace/*
+ok 8 - rev-parse --branches=subspace/
+ok 9 - rev-parse --branches=subspace
+ok 10 - rev-parse --glob=heads/subspace/* --glob=heads/other/*
+ok 11 - rev-parse --glob=heads/someref/* master
+ok 12 - rev-parse --glob=heads/*
+ok 13 - rev-parse --tags=foo
+ok 14 - rev-parse --remotes=foo
+ok 15 - rev-list --glob=refs/heads/subspace/*
+ok 16 - rev-list --glob refs/heads/subspace/*
+ok 17 - rev-list --glob=heads/subspace/*
+ok 18 - rev-list --glob=refs/heads/subspace/
+ok 19 - rev-list --glob=heads/subspace/
+ok 20 - rev-list --glob=heads/subspace
+ok 21 - rev-list --branches=subspace/*
+ok 22 - rev-list --branches=subspace/
+ok 23 - rev-list --branches=subspace
+ok 24 - rev-list --branches
+ok 25 - rev-list --glob=heads/someref/* master
+ok 26 - rev-list --glob=heads/subspace/* --glob=heads/other/*
+ok 27 - rev-list --glob=heads/*
+ok 28 - rev-list --tags=foo
+ok 29 - rev-list --tags
+ok 30 - rev-list --remotes=foo
+# passed all 30 test(s)
+1..30
