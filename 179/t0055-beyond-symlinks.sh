8253ee7bbac6550583bad6fa987dfb67b507e595
smolder on v.nix.is <smolder@v.nix.is>
2010-10-18 02:03:03 +0000

    Bumping smolder reports for Git

---
+++ 179/t0055-beyond-symlinks.sh
@@ -0,0 +1,5 @@
+ok 1 - setup
+ok 2 - update-index --add beyond symlinks
+ok 3 - add beyond symlinks
+# passed all 3 test(s)
+1..3
---
+++ 180/t2015-checkout-unborn.sh
@@ -0,0 +1,6 @@
+ok 1 - setup
+ok 2 - checkout from unborn preserves untracked files
+ok 3 - checkout from unborn preserves index contents
+ok 4 - checkout from unborn merges identical index contents
+# passed all 4 test(s)
+1..4
