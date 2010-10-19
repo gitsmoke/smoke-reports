5ff1f3378918a13faf7d918bba467314c293ca20
smolder on v.nix.is <smolder@v.nix.is>
2010-10-19 02:01:34 +0000

    Bumping smolder reports for Git

---
+++ 183/t4011-diff-symlink.sh
@@ -0,0 +1,8 @@
+ok 1 - diff new symlink
+ok 2 - diff unchanged symlink
+ok 3 - diff removed symlink
+ok 4 - diff identical, but newly created symlink
+ok 5 - diff different symlink
+ok 6 - diff symlinks with non-existing targets
+# passed all 6 test(s)
+1..6
---
+++ 184/t8007-cat-file-textconv.sh
@@ -0,0 +1,12 @@
+ok 1 - setup 
+ok 2 - no filter specified
+ok 3 - setup textconv filters
+ok 4 - cat-file without --textconv
+ok 5 - cat-file without --textconv on previous commit
+ok 6 - cat-file --textconv on last commit
+ok 7 - cat-file --textconv on previous commit
+ok 8 - cat-file without --textconv (symlink)
+ok 9 - cat-file --textconv on index (symlink)
+ok 10 - cat-file --textconv on HEAD (symlink)
+# passed all 10 test(s)
+1..10
