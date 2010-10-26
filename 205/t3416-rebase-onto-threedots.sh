8ef8b157de9e3c88e068873c6c2ed5da9ba02afc
smolder on v.nix.is <smolder@v.nix.is>
2010-10-26 02:01:53 +0000

    Bumping smolder reports for Git

---
+++ 205/t3416-rebase-onto-threedots.sh
@@ -0,0 +1,9 @@
+ok 1 - setup
+ok 2 - rebase --onto master...topic
+ok 3 - rebase --onto master...
+ok 4 - rebase --onto master...side
+ok 5 - rebase -i --onto master...topic
+ok 6 - rebase -i --onto master...
+ok 7 - rebase -i --onto master...side
+# passed all 7 test(s)
+1..7
