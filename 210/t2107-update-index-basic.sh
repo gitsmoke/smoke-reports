544a60730be812531fe488fa7c30229ce65a1ed4
smolder on v.nix.is <smolder@v.nix.is>
2010-10-28 02:01:38 +0000

    Bumping smolder reports for Git

---
+++ 210/t2107-update-index-basic.sh
@@ -0,0 +1,6 @@
+ok 1 - update-index --nonsense fails
+not ok 2 - update-index --nonsense dumps usage # TODO known breakage
+ok 3 - update-index -h with corrupt index
+# still have 1 known breakage(s)
+# passed all remaining 2 test(s)
+1..3
---
+++ 212/t1302-repo-version.sh
@@ -0,0 +1,7 @@
+ok 1 - setup
+ok 2 - gitdir selection on normal repos
+ok 3 - gitdir selection on unsupported repo
+ok 4 - gitdir not required mode
+ok 5 - gitdir required mode
+# passed all 5 test(s)
+1..5
