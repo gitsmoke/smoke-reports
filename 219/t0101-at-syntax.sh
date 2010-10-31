267ab05cc25f21df2773e13edb71d4ecfdeb06c8
smolder on v.nix.is <smolder@v.nix.is>
2010-10-31 02:01:33 +0000

    Bumping smolder reports for Git

---
+++ 219/t0101-at-syntax.sh
@@ -0,0 +1,10 @@
+ok 1 - setup
+ok 2 - @{0} shows current
+ok 3 - @{1} shows old
+ok 4 - @{now} shows current
+ok 5 - @{2001-09-17} (before the first commit) shows old
+ok 6 - silly approxidates work
+ok 7 - notice misspelled upstream
+ok 8 - complain about total nonsense
+# passed all 8 test(s)
+1..8
