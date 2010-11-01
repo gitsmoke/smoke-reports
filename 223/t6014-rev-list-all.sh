b1db2d9ebce3933d86b660b4958a971e64b2624b
smolder on v.nix.is <smolder@v.nix.is>
2010-11-01 04:01:29 +0000

    Bumping smolder reports for Git

---
+++ 223/t6014-rev-list-all.sh
@@ -0,0 +1,5 @@
+ok 1 - setup
+ok 2 - rev-list --all lists detached HEAD
+ok 3 - repack does not lose detached HEAD
+# passed all 3 test(s)
+1..3
---
+++ 224/t1010-mktree.sh
@@ -0,0 +1,10 @@
+ok 1 - setup
+ok 2 - ls-tree piped to mktree (1)
+ok 3 - ls-tree piped to mktree (2)
+ok 4 - ls-tree output in wrong order given to mktree (1)
+ok 5 - ls-tree output in wrong order given to mktree (2)
+ok 6 - allow missing object with --missing
+ok 7 - mktree refuses to read ls-tree -r output (1)
+ok 8 - mktree refuses to read ls-tree -r output (2)
+# passed all 8 test(s)
+1..8
