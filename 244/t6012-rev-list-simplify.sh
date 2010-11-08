ec8f9772d6e56b0c7e0facf9c0e61274e2b60f7c
smolder on v.nix.is <smolder@v.nix.is>
2010-11-08 04:02:36 +0000

    Bumping smolder reports for Git

---
+++ 244/t6012-rev-list-simplify.sh
@@ -0,0 +1,10 @@
+ok 1 - setup
+ok 2 - log --full-history
+ok 3 - log --full-history -- file
+ok 4 - log --full-history --topo-order -- file
+ok 5 - log --full-history --date-order -- file
+ok 6 - log --simplify-merges -- file
+ok 7 - log -- file
+ok 8 - log --topo-order -- file
+# passed all 8 test(s)
+1..8
---
+++ 244/t6016-rev-list-graph-simplify-history.sh
@@ -0,0 +1,13 @@
+ok 1 - set up rev-list --graph test
+ok 2 - --graph --all
+ok 3 - --graph --simplify-by-decoration
+ok 4 - --graph --simplify-by-decoration prune branch B
+ok 5 - --graph --full-history -- bar.txt
+ok 6 - --graph --full-history --simplify-merges -- bar.txt
+ok 7 - --graph -- bar.txt
+ok 8 - --graph --sparse -- bar.txt
+ok 9 - --graph ^C4
+ok 10 - --graph ^C3
+ok 11 - --graph --boundary ^C3
+# passed all 11 test(s)
+1..11
