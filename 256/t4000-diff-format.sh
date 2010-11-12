c1d1bd02e6a92a915cb0560488a5002b91684a83
smolder on v.nix.is <smolder@v.nix.is>
2010-11-12 02:01:49 +0000

    Bumping smolder reports for Git

---
+++ 256/t4000-diff-format.sh
@@ -0,0 +1,5 @@
+ok 1 - update-index --add two files with and without +x.
+ok 2 - git diff-files -p after editing work tree.
+ok 3 - validate git diff-files -p output.
+# passed all 3 test(s)
+1..3
---
+++ 256/t9132-git-svn-broken-symlink.sh
@@ -0,0 +1,7 @@
+ok 1 - load svn dumpfile
+ok 2 - clone using git svn
+ok 3 - "bar" is a symlink that points to "asdf"
+ok 4 - get "bar" => symlink fix from svn
+ok 5 - "bar" remains a proper symlink
+# passed all 5 test(s)
+1..5
