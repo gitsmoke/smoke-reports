4137255d6512a4776e47705bc9312da22fcf04c2
smolder on v.nix.is <smolder@v.nix.is>
2010-11-13 02:02:28 +0000

    Bumping smolder reports for Git

---
+++ 259/t2107-update-index-basic.sh
@@ -0,0 +1,6 @@
+ok 1 - update-index --nonsense fails
+ok 2 - update-index --nonsense dumps usage # TODO known breakage
+ok 3 - update-index -h with corrupt index
+# fixed 1 known breakage(s)
+# passed all 3 test(s)
+1..3
