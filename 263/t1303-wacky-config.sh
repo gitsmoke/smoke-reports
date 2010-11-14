a7732b6aa252885d49a0744fc1b205d3de0ea422
smolder on v.nix.is <smolder@v.nix.is>
2010-11-14 04:02:13 +0000

    Bumping smolder reports for Git

---
+++ 263/t1303-wacky-config.sh
@@ -0,0 +1,7 @@
+ok 1 - modify same key
+ok 2 - add key in same section
+ok 3 - add key in different section
+ok 4 - make sure git config escapes section names properly
+ok 5 - do not crash on special long config line
+# passed all 5 test(s)
+1..5
