ada6ffefc88d2df2db8dab98ba6f15b39babb33f
smolder on v.nix.is <smolder@v.nix.is>
2010-11-16 04:02:30 +0000

    Bumping smolder reports for Git

---
+++ 269/t3415-rebase-autosquash.sh
@@ -0,0 +1,12 @@
+ok 1 - setup
+ok 2 - auto fixup (option)
+ok 3 - auto fixup (config)
+ok 4 - auto squash (option)
+ok 5 - auto squash (config)
+ok 6 - misspelled auto squash
+ok 7 - auto squash that matches 2 commits
+ok 8 - auto squash that matches a commit after the squash
+ok 9 - auto squash that matches a sha1
+ok 10 - auto squash that matches longer sha1
+# passed all 10 test(s)
+1..10
