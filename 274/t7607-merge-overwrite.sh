d7ac49d5e4d42436739c32a2f45ba72521432f67
smolder on v.nix.is <smolder@v.nix.is>
2010-11-17 16:01:43 +0000

    Bumping smolder reports for Git

---
+++ 274/t7607-merge-overwrite.sh
@@ -0,0 +1,9 @@
+ok 1 - setup
+ok 2 - will not overwrite untracked file
+ok 3 - will not overwrite new file
+ok 4 - will not overwrite staged changes
+ok 5 - will not overwrite removed file
+ok 6 - will not overwrite re-added file
+ok 7 - will not overwrite removed file with staged changes
+# passed all 7 test(s)
+1..7
