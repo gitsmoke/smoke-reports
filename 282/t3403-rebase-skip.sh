c4ee852414d3fbd2d9a1b5418266148b8756cf31
smolder on v.nix.is <smolder@v.nix.is>
2010-11-20 02:01:58 +0000

    Bumping smolder reports for Git

---
+++ 282/t3403-rebase-skip.sh
@@ -0,0 +1,11 @@
+ok 1 - setup
+ok 2 - rebase with git am -3 (default)
+ok 3 - rebase --skip with am -3
+ok 4 - rebase moves back to skip-reference
+ok 5 - checkout skip-merge
+ok 6 - rebase with --merge
+ok 7 - rebase --skip with --merge
+ok 8 - merge and reference trees equal
+ok 9 - moved back to branch correctly
+# passed all 9 test(s)
+1..9
---
+++ 282/t5550-http-fetch.sh
@@ -0,0 +1,12 @@
+ok 1 - setup repository
+ok 2 - create http-accessible bare repository
+ok 3 - clone http repository
+ok 4 - clone http repository with authentication
+ok 5 - fetch changes via http
+ok 6 - http remote detects correct HEAD
+ok 7 - fetch packed objects
+ok 8 - fetch notices corrupt pack
+ok 9 - fetch notices corrupt idx
+ok 10 - did not use upload-pack service
+# passed all 10 test(s)
+1..10
