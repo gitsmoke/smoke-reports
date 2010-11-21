f06b9e78914e39005b1e0d438548a35ca7b3c5db
smolder on v.nix.is <smolder@v.nix.is>
2010-11-21 04:03:04 +0000

    Bumping smolder reports for Git

---
+++ 286/t3419-rebase-patch-id.sh
@@ -0,0 +1,11 @@
+ok 1 - setup
+ok 2 - setup: 500 lines
+ok 3 - setup attributes
+ok 4 - detect upstream patch
+ok 5 - do not drop patch
+ok 6 # skip setup: 50000 lines (missing EXPENSIVE)
+ok 7 # skip setup attributes (missing EXPENSIVE)
+ok 8 # skip detect upstream patch (missing EXPENSIVE)
+ok 9 # skip do not drop patch (missing EXPENSIVE)
+# passed all 9 test(s)
+1..9
