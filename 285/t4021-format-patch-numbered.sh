6d17b2bfc6e6fb4f770526e8494e6642dbead8b6
smolder on v.nix.is <smolder@v.nix.is>
2010-11-21 02:03:19 +0000

    Bumping smolder reports for Git

---
+++ 285/t4021-format-patch-numbered.sh
@@ -0,0 +1,14 @@
+ok 1 - setup
+ok 2 - single patch defaults to no numbers
+ok 3 - multiple patch defaults to numbered
+ok 4 - Use --numbered
+ok 5 - format.numbered = true
+ok 6 - format.numbered && single patch
+ok 7 - format.numbered && --no-numbered
+ok 8 - format.numbered && --keep-subject
+ok 9 - format.numbered = auto
+ok 10 - format.numbered = auto && single patch
+ok 11 - format.numbered = auto && --no-numbered
+ok 12 - --start-number && --numbered
+# passed all 12 test(s)
+1..12
