1824df5c8998f348983345447e76034cefc2e4ed
smolder on v.nix.is <smolder@v.nix.is>
2010-12-05 02:02:40 +0000

    Bumping smolder reports for Git

---
+++ 327/t4010-diff-pathspec.sh
@@ -0,0 +1,12 @@
+ok 1 - setup
+ok 2 - limit to path should show nothing
+ok 3 - limit to path1 should show path1/file1
+ok 4 - limit to path1/ should show path1/file1
+ok 5 - limit to file0 should show file0
+ok 6 - diff-index with wildcard
+ok 7 - limit to file0/ should emit nothing.
+ok 8 - diff-tree pathspec
+ok 9 - diff-tree with wildcard shows dir also matches
+ok 10 - diff-tree -r with wildcard
+# passed all 10 test(s)
+1..10
---
+++ 327/t9139-git-svn-non-utf8-commitencoding.sh
@@ -0,0 +1,8 @@
+ok 1 - ISO8859-1 setup
+ok 2 - eucJP setup
+ok 3 - ISO8859-1 commit on git side
+ok 4 - eucJP commit on git side
+ok 5 - ISO8859-1 dcommit to svn
+ok 6 - eucJP dcommit to svn
+# passed all 6 test(s)
+1..6
