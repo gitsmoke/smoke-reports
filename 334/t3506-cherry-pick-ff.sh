222d28b0f152d9282480784bd6e00792d6d86429
smolder on v.nix.is <smolder@v.nix.is>
2010-12-07 04:04:02 +0000

    Bumping smolder reports for Git

---
+++ 334/t3506-cherry-pick-ff.sh
@@ -0,0 +1,12 @@
+ok 1 - setup
+ok 2 - cherry-pick using --ff fast forwards
+ok 3 - cherry-pick not using --ff does not fast forwards
+ok 4 - merge setup
+ok 5 - cherry-pick a non-merge with --ff and -m should fail
+ok 6 - cherry pick a merge with --ff but without -m should fail
+ok 7 - cherry pick with --ff a merge (1)
+ok 8 - cherry pick with --ff a merge (2)
+ok 9 - cherry pick a merge relative to nonexistent parent with --ff should fail
+ok 10 - cherry pick a root commit with --ff
+# passed all 10 test(s)
+1..10
