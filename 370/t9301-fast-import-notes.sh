e0536a72a42176be8cc1fc5a5bb3dc7d51a8cc09
smolder on v.nix.is <smolder@v.nix.is>
2010-12-18 04:03:50 +0000

    Bumping smolder reports for Git

---
+++ 370/t9301-fast-import-notes.sh
@@ -0,0 +1,16 @@
+ok 1 - set up master branch
+ok 2 - add notes with simple M command
+ok 3 - add notes with simple N command
+ok 4 - update existing notes with N command
+ok 5 - add concatentation notes with M command
+ok 6 - verify that deleteall also removes notes
+ok 7 - verify that later N commands override earlier M commands
+ok 8 - add lots of commits and notes
+ok 9 - verify that lots of notes trigger a fanout scheme
+ok 10 - verify that non-notes are untouched by a fanout change
+ok 11 - remove lots of notes
+ok 12 - verify that removing notes trigger fanout consolidation
+ok 13 - verify that non-notes are untouched by a fanout change
+ok 14 - add notes to 16 commits in each of 10 refs
+# passed all 14 test(s)
+1..14
---
+++ 371/t5706-clone-branch.sh
@@ -0,0 +1,8 @@
+ok 1 - setup
+ok 2 - vanilla clone chooses HEAD
+ok 3 - clone -b chooses specified branch
+ok 4 - clone -b sets up tracking
+ok 5 - clone -b does not munge remotes/origin/HEAD
+ok 6 - clone -b with bogus branch chooses HEAD
+# passed all 6 test(s)
+1..6
