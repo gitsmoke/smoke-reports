a65d31a67d0aea3f0b28f30e2efd0e4ef1397e1e
smolder on v.nix.is <smolder@v.nix.is>
2010-12-20 02:02:57 +0000

    Bumping smolder reports for Git

---
+++ 375/t4120-apply-popt.sh
@@ -0,0 +1,9 @@
+ok 1 - setup
+ok 2 - apply git diff with -p2
+ok 3 - apply with too large -p
+ok 4 - apply (-p2) traditional diff with funny filenames
+ok 5 - apply with too large -p and fancy filename
+ok 6 - apply (-p2) diff, mode change only
+ok 7 - apply (-p2) diff, rename
+# passed all 7 test(s)
+1..7
