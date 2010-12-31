bc3348b3e85b8eb7230dd2de4bfe0fb4276765d6
smolder on v.nix.is <smolder@v.nix.is>
2010-12-31 04:00:27 +0000

    Bumping smolder reports for Git

---
+++ 404/t3003-ls-files-exclude.sh
@@ -0,0 +1,9 @@
+ok 1 - create repo with file
+ok 2 - ls-files output contains file (cached)
+ok 3 - ls-files output contains file (modified)
+ok 4 - add file to gitignore
+ok 5 - ls-files output contains file (cached)
+ok 6 - ls-files output contains file (modified)
+ok 7 - ls-files -i lists only tracked-but-ignored files
+# passed all 7 test(s)
+1..7
