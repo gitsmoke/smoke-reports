0fe7812eef1bb3af9cf28aaee4e1600e85fed9c2
smolder on v.nix.is <smolder@v.nix.is>
2011-01-02 04:01:33 +0000

    Bumping smolder reports for Git

---
+++ 411/t4115-apply-symlink.sh
@@ -0,0 +1,5 @@
+ok 1 - setup
+ok 2 - apply symlink patch
+ok 3 - apply --index symlink patch
+# passed all 3 test(s)
+1..3
---
+++ 412/t3302-notes-index-expensive.sh
@@ -0,0 +1,22 @@
+ok 1 - setup / mkdir
+ok 2 - setup 10
+ok 3 - notes work
+ok 4 - notes timing with /usr/bin/time
+ok 5 - teardown / cd ..
+ok 6 # skip setup / mkdir (missing EXPENSIVE)
+ok 7 # skip setup 100 (missing EXPENSIVE)
+ok 8 # skip notes work (missing EXPENSIVE)
+ok 9 # skip notes timing with /usr/bin/time (missing EXPENSIVE of USR_BIN_TIME,EXPENSIVE)
+ok 10 # skip teardown / cd .. (missing EXPENSIVE)
+ok 11 # skip setup / mkdir (missing EXPENSIVE)
+ok 12 # skip setup 1000 (missing EXPENSIVE)
+ok 13 # skip notes work (missing EXPENSIVE)
+ok 14 # skip notes timing with /usr/bin/time (missing EXPENSIVE of USR_BIN_TIME,EXPENSIVE)
+ok 15 # skip teardown / cd .. (missing EXPENSIVE)
+ok 16 # skip setup / mkdir (missing EXPENSIVE)
+ok 17 # skip setup 10000 (missing EXPENSIVE)
+ok 18 # skip notes work (missing EXPENSIVE)
+ok 19 # skip notes timing with /usr/bin/time (missing EXPENSIVE of USR_BIN_TIME,EXPENSIVE)
+ok 20 # skip teardown / cd .. (missing EXPENSIVE)
+# passed all 20 test(s)
+1..20
