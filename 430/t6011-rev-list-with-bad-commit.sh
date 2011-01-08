878872091a27623dbf119afa72de9d8c57e07e6f
smolder on v.nix.is <smolder@v.nix.is>
2011-01-08 04:01:33 +0000

    Bumping smolder reports for Git

---
+++ 430/t6011-rev-list-with-bad-commit.sh
@@ -0,0 +1,8 @@
+ok 1 - setup
+ok 2 - verify number of revisions
+ok 3 - corrupt second commit object
+ok 4 - rev-list should fail
+ok 5 - git repack _MUST_ fail
+ok 6 - first commit is still available
+# passed all 6 test(s)
+1..6
