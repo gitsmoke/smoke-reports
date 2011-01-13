3410fe733c0ef8742914b71a2c2ea191728c78c7
smolder on v.nix.is <smolder@v.nix.is>
2011-01-13 02:00:31 +0000

    Bumping smolder reports for Git

---
+++ 444/t0004-unwritable.sh
@@ -0,0 +1,7 @@
+ok 1 - setup
+ok 2 # skip write-tree should notice unwritable repository (missing SANITY of POSIXPERM,SANITY)
+ok 3 # skip commit should notice unwritable repository (missing SANITY of POSIXPERM,SANITY)
+ok 4 # skip update-index should notice unwritable repository (missing SANITY of POSIXPERM,SANITY)
+ok 5 # skip add should notice unwritable repository (missing SANITY of POSIXPERM,SANITY)
+# passed all 5 test(s)
+1..5
---
+++ 444/t9010-svn-fe.sh
@@ -0,0 +1,5 @@
+ok 1 - empty dump
+ok 2 - v3 dumps not supported
+ok 3 - t9135/svn.dump
+# passed all 3 test(s)
+1..3
