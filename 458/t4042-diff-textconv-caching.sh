17805953333555f23573fd214cadce9aa59f056f
smolder on v.nix.is <smolder@v.nix.is>
2011-01-15 04:01:05 +0000

    Bumping smolder reports for Git

---
+++ 458/t4042-diff-textconv-caching.sh
@@ -0,0 +1,8 @@
+ok 1 - setup
+ok 2 - first textconv works
+ok 3 - cached textconv produces same output
+ok 4 - cached textconv does not run helper
+ok 5 - changing textconv invalidates cache
+ok 6 - switching diff driver produces correct results
+# passed all 6 test(s)
+1..6
