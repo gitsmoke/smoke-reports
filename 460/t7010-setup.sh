20f07103ced92844ab5563aaa0da41a30d3340ee
smolder on v.nix.is <smolder@v.nix.is>
2011-01-16 04:00:23 +0000

    Bumping smolder reports for Git

---
+++ 460/t7010-setup.sh
@@ -0,0 +1,18 @@
+ok 1 - setup
+ok 2 - git add (absolute)
+ok 3 - git add (funny relative)
+ok 4 - git rm (absolute)
+ok 5 - git rm (funny relative)
+ok 6 - git ls-files (absolute)
+ok 7 - git ls-files (relative #1)
+ok 8 - git ls-files (relative #2)
+ok 9 - git ls-files (relative #3)
+ok 10 - commit using absolute path names
+ok 11 - log using absolute path names
+ok 12 - blame using absolute path names
+ok 13 - setup deeper work tree
+ok 14 - add a directory outside the work tree
+ok 15 - add a file outside the work tree, nasty case 1
+ok 16 - add a file outside the work tree, nasty case 2
+# passed all 16 test(s)
+1..16
