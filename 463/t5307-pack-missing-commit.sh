cbada83a1fa825bfd60dd8d92d817919daf738f4
smolder on v.nix.is <smolder@v.nix.is>
2011-01-17 04:00:45 +0000

    Bumping smolder reports for Git

---
+++ 463/t5307-pack-missing-commit.sh
@@ -0,0 +1,7 @@
+ok 1 - setup
+ok 2 - check corruption
+ok 3 - rev-list notices corruption (1)
+ok 4 - rev-list notices corruption (2)
+ok 5 - pack-objects notices corruption
+# passed all 5 test(s)
+1..5
