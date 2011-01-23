0655001492e75719b4535dfa4cd4919b43d1777f
smolder on v.nix.is <smolder@v.nix.is>
2011-01-23 04:00:27 +0000

    Bumping smolder reports for Git

---
+++ 476/t3407-rebase-abort.sh
@@ -0,0 +1,13 @@
+ok 1 - setup
+ok 2 - rebase --abort
+ok 3 - rebase --abort after --skip
+ok 4 - rebase --abort after --continue
+ok 5 - rebase --abort does not update reflog
+ok 6 - rebase --abort can not be used with other options
+ok 7 - rebase --merge --abort
+ok 8 - rebase --merge --abort after --skip
+ok 9 - rebase --merge --abort after --continue
+ok 10 - rebase --merge --abort does not update reflog
+ok 11 - rebase --abort can not be used with other options
+# passed all 11 test(s)
+1..11
