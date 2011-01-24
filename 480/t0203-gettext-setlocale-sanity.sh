0ff0e98c89283e0a575f862d5fd9a0335935d991
smolder on v.nix.is <smolder@v.nix.is>
2011-01-24 06:00:27 +0000

    Bumping smolder reports for Git

---
+++ 480/t0203-gettext-setlocale-sanity.sh
@@ -0,0 +1,5 @@
+# lib-gettext: GETTEXT_POISON defined, can't test gettext
+ok 1 - git show a ISO-8859-1 commit under C locale
+ok 2 # skip git show a ISO-8859-1 commit under a UTF-8 locale (missing GETTEXT_LOCALE)
+# passed all 2 test(s)
+1..2
