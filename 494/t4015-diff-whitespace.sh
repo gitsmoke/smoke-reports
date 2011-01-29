74ddfef184c7a51d4160d75026a20fb0987e9828
smolder on v.nix.is <smolder@v.nix.is>
2011-01-29 04:00:46 +0000

    Bumping smolder reports for Git

---
+++ 494/t4015-diff-whitespace.sh
@@ -0,0 +1,58 @@
+ok 1 - Ray's example without options
+ok 2 - Ray's example with -w
+ok 3 - Ray's example with -b
+ok 4 - another test, without options
+ok 5 - another test, with -w
+ok 6 - another test, with -w -b
+ok 7 - another test, with -w --ignore-space-at-eol
+ok 8 - another test, with -w -b --ignore-space-at-eol
+ok 9 - another test, with -b
+ok 10 - another test, with -b --ignore-space-at-eol
+ok 11 - another test, with --ignore-space-at-eol
+ok 12 - check mixed spaces and tabs in indent
+ok 13 - check mixed tabs and spaces in indent
+ok 14 - check with no whitespace errors
+ok 15 - check with trailing whitespace
+ok 16 - check with space before tab in indent
+ok 17 - --check and --exit-code are not exclusive
+ok 18 - --check and --quiet are not exclusive
+ok 19 - check staged with no whitespace errors
+ok 20 - check staged with trailing whitespace
+ok 21 - check staged with space before tab in indent
+ok 22 - check with no whitespace errors (diff-index)
+ok 23 - check with trailing whitespace (diff-index)
+ok 24 - check with space before tab in indent (diff-index)
+ok 25 - check staged with no whitespace errors (diff-index)
+ok 26 - check staged with trailing whitespace (diff-index)
+ok 27 - check staged with space before tab in indent (diff-index)
+ok 28 - check with no whitespace errors (diff-tree)
+ok 29 - check with trailing whitespace (diff-tree)
+ok 30 - check with space before tab in indent (diff-tree)
+ok 31 - check trailing whitespace (trailing-space: off)
+ok 32 - check trailing whitespace (trailing-space: on)
+ok 33 - check space before tab in indent (space-before-tab: off)
+ok 34 - check space before tab in indent (space-before-tab: on)
+ok 35 - check spaces as indentation (indent-with-non-tab: off)
+ok 36 - check spaces as indentation (indent-with-non-tab: on)
+ok 37 - ditto, but tabwidth=9
+ok 38 - check tabs and spaces as indentation (indent-with-non-tab: on)
+ok 39 - ditto, but tabwidth=10
+ok 40 - ditto, but tabwidth=20
+ok 41 - check tabs as indentation (tab-in-indent: off)
+ok 42 - check tabs as indentation (tab-in-indent: on)
+ok 43 - check tabs and spaces as indentation (tab-in-indent: on)
+ok 44 - ditto, but tabwidth=1 (must be irrelevant)
+ok 45 - check tab-in-indent and indent-with-non-tab conflict
+ok 46 - check tab-in-indent excluded from wildcard whitespace attribute
+ok 47 - line numbers in --check output are correct
+ok 48 - checkdiff detects new trailing blank lines (1)
+ok 49 - checkdiff detects new trailing blank lines (2)
+ok 50 - checkdiff allows new blank lines
+ok 51 - whitespace-only changes not reported
+ok 52 - whitespace-only changes reported across renames
+ok 53 - rename empty
+ok 54 - combined diff with autocrlf conversion
+ok 55 - setup diff colors
+ok 56 - diff that introduces a line with only tabs
+# passed all 56 test(s)
+1..56
---
+++ 494/t4030-diff-textconv.sh
@@ -0,0 +1,13 @@
+ok 1 - setup binary file with history
+ok 2 - file is considered binary by porcelain
+ok 3 - file is considered binary by plumbing
+ok 4 - setup textconv filters
+ok 5 - diff produces text
+ok 6 - diff-tree produces binary
+ok 7 - log produces text
+ok 8 - format-patch produces binary
+ok 9 - status -v produces text
+ok 10 - diffstat does not run textconv
+ok 11 - textconv does not act on symlinks
+# passed all 11 test(s)
+1..11
