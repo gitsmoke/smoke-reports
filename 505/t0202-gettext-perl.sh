f3ced96d781dfaa6cd7cd19fcc0ac3d55ae2dc82
smolder on v.nix.is <smolder@v.nix.is>
2011-02-01 04:00:30 +0000

    Bumping smolder reports for Git

---
+++ 505/t0202-gettext-perl.sh
@@ -0,0 +1,15 @@
+# lib-gettext: Found 'is_IS.utf8' as a is_IS UTF-8 locale
+# lib-gettext: Found 'is_IS.iso88591' as a is_IS ISO-8859-1 locale
+# run 1: Perl Git::I18N API (/usr/bin/perl /local/home/trast/git-smoke/t/t0202/test.pl)
+1..9
+ok 1 - Testing Git::I18N version 0.01 with NO Perl gettext library
+ok 2 - Git::I18N is located at /local/home/trast/git-smoke/t/../perl/blib/lib/Git/I18N.pm
+ok 3 - sanity: Git::I18N defines a $VERSION
+ok 4 - sanity: Git::I18N has 1 export(s)
+ok 5 - sanity: Git::I18N exports everything by default
+ok 6 - sanity: __ has a $ prototype
+ok 7 - Passing a string through __() in the C locale works
+ok 8 - Without a gettext library + <C> locale <TEST: A Perl test string> turns into <TEST: A Perl test string>
+ok 9 - Without a gettext library + <is> locale <TEST: A Perl test string> turns into <TEST: A Perl test string>
+# test_external test Perl Git::I18N API was ok
+# test_external_without_stderr test no stderr: Perl Git::I18N API was ok
---
+++ 505/t6040-tracking-info.sh
@@ -0,0 +1,11 @@
+ok 1 - setup
+ok 2 - branch -v
+ok 3 - checkout
+ok 4 - checkout with local tracked branch
+ok 5 - status
+ok 6 - status when tracking lightweight tags
+ok 7 - status when tracking annotated tags
+ok 8 - setup tracking with branch --set-upstream on existing branch
+ok 9 - --set-upstream does not change branch
+# passed all 9 test(s)
+1..9
