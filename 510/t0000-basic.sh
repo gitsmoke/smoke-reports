e97de4864c57747d8eae468f77ad6f89e7bfeb63
smolder on v.nix.is <smolder@v.nix.is>
2011-02-02 04:03:32 +0000

    Bumping smolder reports for Git

---
+++ 510/t0000-basic.sh
@@ -0,0 +1,51 @@
+ok 1 - .git/objects should be empty after git init in an empty repo.
+ok 2 - .git/objects should have 3 subdirectories.
+ok 3 - success is reported like this
+not ok 4 - pretend we have a known breakage # TODO known breakage
+ok 5 - pretend we have fixed a known breakage (run in sub test-lib)
+ok 6 - test runs if prerequisite is satisfied
+ok 7 # skip unmet prerequisite causes test to be skipped (missing DONTHAVEIT)
+ok 8 - test runs if prerequisites are satisfied
+ok 9 # skip unmet prerequisites causes test to be skipped (missing DONTHAVEIT of HAVEIT,DONTHAVEIT)
+ok 10 # skip unmet prerequisites causes test to be skipped (missing DONTHAVEIT of DONTHAVEIT,HAVEIT)
+ok 11 - tests clean up after themselves
+ok 12 - tests clean up even on failures
+ok 13 - git update-index without --add should fail adding.
+ok 14 - git update-index with --add should succeed.
+ok 15 - writing tree out with git write-tree
+ok 16 - validate object ID of a known tree.
+ok 17 - git update-index without --remove should fail removing.
+ok 18 - git update-index with --remove should be able to remove.
+ok 19 - git write-tree should be able to write an empty tree.
+ok 20 - validate object ID of a known tree.
+ok 21 - adding various types of objects with git update-index --add.
+ok 22 - showing stage with git ls-files --stage
+ok 23 - validate git ls-files output for a known tree.
+ok 24 - writing tree out with git write-tree.
+ok 25 - validate object ID for a known tree.
+ok 26 - showing tree with git ls-tree
+ok 27 - git ls-tree output for a known tree.
+ok 28 - showing tree with git ls-tree -r
+ok 29 - git ls-tree -r output for a known tree.
+ok 30 - showing tree with git ls-tree -r -t
+ok 31 - git ls-tree -r output for a known tree.
+ok 32 - writing partial tree out with git write-tree --prefix.
+ok 33 - validate object ID for a known tree.
+ok 34 - writing partial tree out with git write-tree --prefix.
+ok 35 - validate object ID for a known tree.
+ok 36 - put invalid objects into the index.
+ok 37 - writing this tree without --missing-ok.
+ok 38 - writing this tree with --missing-ok.
+ok 39 - git read-tree followed by write-tree should be idempotent.
+ok 40 - validate git diff-files output for a know cache/work tree state.
+ok 41 - git update-index --refresh should succeed.
+ok 42 - no diff after checkout and git update-index --refresh.
+ok 43 - git commit-tree records the correct tree in a commit.
+ok 44 - git commit-tree records the correct parent in a commit.
+ok 45 - git commit-tree omits duplicated parent in a commit.
+ok 46 - update-index D/F conflict
+ok 47 - absolute path works as expected
+ok 48 - very long name in the index handled sanely
+# still have 1 known breakage(s)
+# passed all remaining 47 test(s)
+1..48
---
+++ 510/t6032-merge-large-rename.sh
@@ -0,0 +1,12 @@
+ok 1 - setup (initial)
+ok 2 - rename (5, ok)
+ok 3 - set diff.renamelimit to 4
+ok 4 - rename (4, ok)
+ok 5 - rename (5, fail)
+ok 6 - set merge.renamelimit to 5
+ok 7 - rename (5, ok)
+ok 8 - rename (6, fail)
+ok 9 - setup large simple rename
+ok 10 - massive simple rename does not spam added files
+# passed all 10 test(s)
+1..10
