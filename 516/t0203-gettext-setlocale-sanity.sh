5c7884a7a0c18e5d7fa43b46f1112b07fabe06c5
smolder on v.nix.is <smolder@v.nix.is>
2011-02-04 04:02:04 +0000

    Bumping smolder reports for Git

---
+++ 516/t0203-gettext-setlocale-sanity.sh
@@ -0,0 +1,6 @@
+# lib-gettext: Found 'is_IS.utf8' as a is_IS UTF-8 locale
+# lib-gettext: Found 'is_IS.iso88591' as a is_IS ISO-8859-1 locale
+ok 1 - git show a ISO-8859-1 commit under C locale
+ok 2 - git show a ISO-8859-1 commit under a UTF-8 locale
+# passed all 2 test(s)
+1..2
