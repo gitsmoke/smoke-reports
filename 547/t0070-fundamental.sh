71f3a73bcc6f3cd5a7f8f8f5d1bc3c8e258e5d04
smolder on v.nix.is <smolder@v.nix.is>
2011-02-10 06:00:30 +0000

    Bumping smolder reports for Git

---
+++ 547/t0070-fundamental.sh
@@ -0,0 +1,5 @@
+ok 1 - character classes (isspace, isalpha etc.)
+ok 2 - mktemp to nonexistent directory prints filename
+ok 3 - mktemp to unwritable directory prints filename
+# passed all 3 test(s)
+1..3
---
+++ 548/t9137-git-svn-dcommit-clobber-series.sh
@@ -0,0 +1,7 @@
+ok 1 - initialize repo
+ok 2 - (supposedly) non-conflicting change from SVN
+ok 3 - some unrelated changes to git
+ok 4 - change file but in unrelated area
+ok 5 - attempt to dcommit with a dirty index
+# passed all 5 test(s)
+1..5
