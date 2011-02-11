93f55f8798b69cbffde30842f2f09c8903e545cd
smolder on v.nix.is <smolder@v.nix.is>
2011-02-11 04:00:43 +0000

    Bumping smolder reports for Git

---
+++ 550/t5407-post-rewrite-hook.sh
@@ -0,0 +1,15 @@
+ok 1 - setup
+ok 2 - git commit --amend
+ok 3 - git commit --amend --no-post-rewrite
+ok 4 - git rebase
+ok 5 - git rebase --skip
+ok 6 - git rebase --skip the last one
+ok 7 - git rebase -m
+ok 8 - git rebase -m --skip
+ok 9 - git rebase -i (unchanged)
+ok 10 - git rebase -i (skip)
+ok 11 - git rebase -i (squash)
+ok 12 - git rebase -i (fixup without conflict)
+ok 13 - git rebase -i (double edit)
+# passed all 13 test(s)
+1..13
