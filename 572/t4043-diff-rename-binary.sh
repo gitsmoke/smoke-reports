97dcc019989be9042756e0178534db4883b3fdb0
smolder on v.nix.is <smolder@v.nix.is>
2011-02-16 10:01:16 +0000

    Bumping smolder reports for Git

---
+++ 572/t4043-diff-rename-binary.sh
@@ -0,0 +1,5 @@
+ok 1 - prepare repository
+ok 2 - move the files into a "sub" directory
+ok 3 - git show -C -C report renames
+# passed all 3 test(s)
+1..3
---
+++ 572/t7006-pager.sh
@@ -0,0 +1,11 @@
+ok 1 - determine default pager
+ok 2 - set up terminal for tests
+ok 3 - setup
+ok 4 - some commands use a pager
+not ok 5 - pager runs from subdir # TODO known breakage
+ok 6 - some commands do not use a pager
+ok 7 - no pager when stdout is a pipe
+ok 8 - no pager when stdout is a regular file
+ok 9 - git --paginate rev-list uses a pager
+ok 10 - no pager even with --paginate when stdout is a pipe
+FATAL: Unexpected exit with code 0
