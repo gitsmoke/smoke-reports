886bee0c76007c3b4625c7573358b750aee28bb3
smolder on v.nix.is <smolder@v.nix.is>
2011-02-19 06:00:26 +0000

    Bumping smolder reports for Git

---
+++ 583/t7505-prepare-commit-msg-hook.sh
@@ -0,0 +1,14 @@
+ok 1 - with no hook
+ok 2 - with hook (-m)
+ok 3 - with hook (-m editor)
+ok 4 - with hook (-t)
+ok 5 - with hook (-F)
+ok 6 - with hook (-F editor)
+ok 7 - with hook (-C)
+ok 8 - with hook (editor)
+ok 9 - with hook (--amend)
+ok 10 - with hook (-c)
+ok 11 - with failing hook
+ok 12 - with failing hook (--no-verify)
+# passed all 12 test(s)
+1..12
