9d4e1dc29e8dcea697ffda352034bdf6c3e70a0e
smolder on v.nix.is <smolder@v.nix.is>
2011-02-20 06:00:35 +0000

    Bumping smolder reports for Git

---
+++ 587/t3904-stash-patch.sh
@@ -0,0 +1,7 @@
+ok 1 - setup
+ok 2 - saying "n" does nothing
+ok 3 - git stash -p
+ok 4 - git stash -p --no-keep-index
+ok 5 - none of this moved HEAD
+# passed all 5 test(s)
+1..5
---
+++ 588/t1420-lost-found.sh
@@ -0,0 +1,4 @@
+ok 1 - setup
+ok 2 - lost and found something
+# passed all 2 test(s)
+1..2
