074948f6a9b3a022fccb23c1156195c71e779721
smolder on v.nix.is <smolder@v.nix.is>
2011-02-21 06:00:48 +0000

    Bumping smolder reports for Git

---
+++ 591/t4127-apply-same-fn.sh
@@ -0,0 +1,8 @@
+ok 1 - setup
+ok 2 - apply same filename with independent changes
+ok 3 - apply same filename with overlapping changes
+ok 4 - apply same new filename after rename
+ok 5 - apply same old filename after rename -- should fail.
+ok 6 - apply A->B (rename), C->A (rename), A->A -- should pass.
+# passed all 6 test(s)
+1..6
