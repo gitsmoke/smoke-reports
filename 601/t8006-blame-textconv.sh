ef1293762906dc728e9065e1562f2984bec1bcbd
smolder on v.nix.is <smolder@v.nix.is>
2011-02-24 04:00:33 +0000

    Bumping smolder reports for Git

---
+++ 601/t8006-blame-textconv.sh
@@ -0,0 +1,17 @@
+ok 1 - setup 
+ok 2 - no filter specified
+ok 3 - setup textconv filters
+ok 4 - blame with --no-textconv
+ok 5 - basic blame on last commit
+ok 6 - blame --textconv going through revisions
+ok 7 - setup +cachetextconv
+ok 8 - blame --textconv works with textconvcache
+ok 9 - setup -cachetextconv
+ok 10 - make a new commit
+ok 11 - blame from previous revision
+ok 12 - blame with --no-textconv (on symlink)
+ok 13 - blame --textconv (on symlink)
+ok 14 - make another new commit
+ok 15 - blame on last commit (-C -C, symlink)
+# passed all 15 test(s)
+1..15
---
+++ 602/t1007-hash-object.sh
@@ -0,0 +1,31 @@
+ok 1 - multiple '--stdin's are rejected
+ok 2 - Can't use --stdin and --stdin-paths together
+ok 3 - Can't pass filenames as arguments with --stdin-paths
+ok 4 - Can't use --path with --stdin-paths
+ok 5 - Can't use --path with --no-filters
+ok 6 - hash a file
+ok 7 - blob does not exist in database
+ok 8 - hash from stdin
+ok 9 - blob does not exist in database
+ok 10 - hash a file and write to database
+ok 11 - blob exists in database
+ok 12 - git hash-object --stdin file1 <file0 first operates on file0, then file1
+ok 13 - check that appropriate filter is invoke when --path is used
+ok 14 - check that --no-filters option works
+ok 15 - check that --no-filters option works with --stdin-paths
+ok 16 - hash from stdin and write to database (-w --stdin)
+ok 17 - blob exists in database
+ok 18 - hash from stdin and write to database (--stdin -w)
+ok 19 - blob exists in database
+ok 20 - hash two files with names on stdin
+ok 21 - hash two files with names on stdin and write to database (-w --stdin-paths)
+ok 22 - blob exists in database
+ok 23 - blob exists in database
+ok 24 - hash two files with names on stdin and write to database (--stdin-paths -w)
+ok 25 - blob exists in database
+ok 26 - blob exists in database
+ok 27 - corrupt tree
+ok 28 - corrupt commit
+ok 29 - corrupt tag
+# passed all 29 test(s)
+1..29
