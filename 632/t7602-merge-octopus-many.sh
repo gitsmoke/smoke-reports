ab5443a9836c61fcb9e6ab84f73808063487d4d7
smolder on v.nix.is <smolder@v.nix.is>
2011-03-03 06:00:46 +0000

    Bumping smolder reports for Git

---
+++ 632/t7602-merge-octopus-many.sh
@@ -0,0 +1,7 @@
+ok 1 - setup
+ok 2 - merge c1 with c2, c3, c4, ... c29
+ok 3 - merge output uses pretty names
+ok 4 - merge up-to-date output uses pretty names
+ok 5 - merge fast-forward output uses pretty names
+# passed all 5 test(s)
+1..5
---
+++ 633/t6500-gc.sh
@@ -0,0 +1,5 @@
+ok 1 - gc empty repository
+ok 2 - gc --gobbledegook
+ok 3 - gc -h with invalid configuration
+# passed all 3 test(s)
+1..3
