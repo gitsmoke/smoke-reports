f428cf65e242b4ee01b67fcbff6cea960ce2c7f7
smolder on v.nix.is <smolder@v.nix.is>
2011-03-11 04:00:58 +0000

    Bumping smolder reports for Git

---
+++ 662/t3505-cherry-pick-empty.sh
@@ -0,0 +1,7 @@
+ok 1 - setup
+ok 2 - cherry-pick an empty commit
+ok 3 - index lockfile was removed
+ok 4 - cherry-pick a commit with an empty message
+ok 5 - index lockfile was removed
+# passed all 5 test(s)
+1..5
