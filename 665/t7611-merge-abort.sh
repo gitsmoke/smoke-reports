f3c16f8a37b1721974ec94c2ac1d612e592454c8
smolder on v.nix.is <smolder@v.nix.is>
2011-03-11 06:00:57 +0000

    Bumping smolder reports for Git

---
+++ 665/t7611-merge-abort.sh
@@ -0,0 +1,30 @@
+ok 1 - setup
+ok 2 - fails without MERGE_HEAD (unstarted merge)
+ok 3 - fails without MERGE_HEAD (unstarted merge): fatal output
+ok 4 - fails without MERGE_HEAD (unstarted merge): .git/MERGE_HEAD sanity
+ok 5 - fails without MERGE_HEAD (completed merge)
+ok 6 - fails without MERGE_HEAD (completed merge): output
+ok 7 - fails without MERGE_HEAD (completed merge): .git/MERGE_HEAD sanity
+ok 8 - Forget previous merge
+ok 9 - Abort after --no-commit
+ok 10 - Abort after conflicts
+ok 11 - Clean merge with dirty index fails
+ok 12 - Conflicting merge with dirty index fails
+ok 13 - Reset index (but preserve worktree changes)
+ok 14 - Abort clean merge with non-conflicting dirty worktree
+ok 15 - Abort conflicting merge with non-conflicting dirty worktree
+ok 16 - Reset worktree changes
+ok 17 - Fail clean merge with conflicting dirty worktree
+ok 18 - Fail conflicting merge with conflicting dirty worktree
+ok 19 - Reset worktree changes
+ok 20 - Fail clean merge with matching dirty worktree
+ok 21 - Abort clean merge with matching dirty index
+ok 22 - Reset worktree changes
+ok 23 - Fail conflicting merge with matching dirty worktree
+ok 24 - Abort conflicting merge with matching dirty index
+ok 25 - Reset worktree changes
+ok 26 - Abort merge with pre- and post-merge worktree changes
+ok 27 - Reset worktree changes
+ok 28 - Abort merge with pre- and post-merge index changes
+# passed all 28 test(s)
+1..28
