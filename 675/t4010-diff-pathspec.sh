c517e28a9037d6c6b1bbd632a166fc4e32210715
smolder on v.nix.is <smolder@v.nix.is>
2011-03-14 04:01:04 +0000

    Bumping smolder reports for Git

---
+++ 675/t4010-diff-pathspec.sh
@@ -0,0 +1,14 @@
+ok 1 - setup
+ok 2 - limit to path should show nothing
+ok 3 - limit to path1 should show path1/file1
+ok 4 - limit to path1/ should show path1/file1
+ok 5 - limit to file0 should show file0
+ok 6 - limit to file0/ should emit nothing.
+ok 7 - diff-tree pathspec
+ok 8 - diff-tree with wildcard shows dir also matches
+ok 9 - diff-tree -r with wildcard
+ok 10 - diff-tree with wildcard shows dir also matches
+ok 11 - diff-tree -r with wildcard from beginning
+ok 12 - diff-tree -r with wildcard
+# passed all 12 test(s)
+1..12
