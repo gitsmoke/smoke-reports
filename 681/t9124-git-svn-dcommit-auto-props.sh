ba4fb90d1ba0757c7eab1ed3ed9fc6544144d2c1
smolder on v.nix.is <smolder@v.nix.is>
2011-03-15 06:00:34 +0000

    Bumping smolder reports for Git

---
+++ 681/t9124-git-svn-dcommit-auto-props.sh
@@ -0,0 +1,9 @@
+ok 1 - initialize git svn
+ok 2 - enable auto-props config
+ok 3 - add files matching auto-props
+ok 4 - disable auto-props config
+ok 5 - add files matching disabled auto-props
+ok 6 - check resulting svn repository
+ok 7 - check renamed file
+# passed all 7 test(s)
+1..7
