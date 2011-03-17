efea6a21f60e136df7e08d5054057de114173f52
smolder on v.nix.is <smolder@v.nix.is>
2011-03-17 04:00:55 +0000

    Bumping smolder reports for Git

---
+++ 687/t5406-remote-rejects.sh
@@ -0,0 +1,5 @@
+ok 1 - setup
+ok 2 - push reports error
+ok 3 - individual ref reports error
+# passed all 3 test(s)
+1..3
---
+++ 687/t9010-svn-fe.sh
@@ -0,0 +1,21 @@
+ok 1 - empty dump
+ok 2 - v4 dumps not supported
+not ok 3 - empty revision # TODO known breakage
+ok 4 - empty properties
+ok 5 - author name and commit message
+ok 6 - unsupported properties are ignored
+not ok 7 - timestamp and empty file # TODO known breakage
+ok 8 - directory with files
+ok 9 - node without action
+ok 10 - action: add node without text
+not ok 11 - change file mode but keep old content # TODO known breakage
+ok 12 - change file mode and reiterate content
+ok 13 - deltas not supported
+ok 14 - property deltas supported
+ok 15 - properties on /
+ok 16 - deltas for typechange
+ok 17 - set up svn repo
+ok 18 - t9135/svn.dump
+# still have 3 known breakage(s)
+# passed all remaining 15 test(s)
+1..18
