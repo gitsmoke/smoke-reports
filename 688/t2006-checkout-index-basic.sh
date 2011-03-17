e9f84acba1457cdd49597d417c6c502e8fa85616
smolder on v.nix.is <smolder@v.nix.is>
2011-03-17 06:00:46 +0000

    Bumping smolder reports for Git

---
+++ 688/t2006-checkout-index-basic.sh
@@ -0,0 +1,4 @@
+ok 1 - checkout-index --gobbledegook
+ok 2 - checkout-index -h in broken repository
+# passed all 2 test(s)
+1..2
---
+++ 689/t9116-git-svn-log.sh
@@ -0,0 +1,17 @@
+ok 1 - setup repository and import
+ok 2 - run log
+ok 3 - run log against a from trunk
+ok 4 - test ascending revision range
+ok 5 - test descending revision range
+ok 6 - test ascending revision range with unreachable revision
+ok 7 - test descending revision range with unreachable revision
+ok 8 - test ascending revision range with unreachable upper boundary revision and 1 commit
+ok 9 - test descending revision range with unreachable upper boundary revision and 1 commit
+ok 10 - test ascending revision range with unreachable lower boundary revision and 1 commit
+ok 11 - test descending revision range with unreachable lower boundary revision and 1 commit
+ok 12 - test ascending revision range with unreachable boundary revisions and no commits
+ok 13 - test descending revision range with unreachable boundary revisions and no commits
+ok 14 - test ascending revision range with unreachable boundary revisions and 1 commit
+ok 15 - test descending revision range with unreachable boundary revisions and 1 commit
+# passed all 15 test(s)
+1..15
