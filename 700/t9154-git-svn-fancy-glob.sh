7cecbc3746b453cf3b999d98d3fa3b0038a0f562
smolder on v.nix.is <smolder@v.nix.is>
2011-03-20 06:01:45 +0000

    Bumping smolder reports for Git

---
+++ 700/t9154-git-svn-fancy-glob.sh
@@ -0,0 +1,6 @@
+ok 1 - load svn repo
+ok 2 - add red branch
+ok 3 - add green branch
+ok 4 - add all branches
+# passed all 4 test(s)
+1..4
---
+++ 701/t9200-git-cvsexportcommit.sh
@@ -0,0 +1,16 @@
+ok 1 - New file
+ok 2 - Remove two files, add two and update two
+ok 3 - Fail to change binary more than one generation old
+ok 4 - Remove only binary files
+ok 5 - Remove only a text file
+ok 6 - New file with spaces in file name
+ok 7 - Update file with spaces in file name
+ok 8 - Mismatching patch should fail
+ok 9 - Retain execute bit
+ok 10 - -w option should work with relative GIT_DIR
+ok 11 - check files before directories
+ok 12 - re-commit a removed filename which remains in CVS attic
+ok 13 - commit a file with leading spaces in the name
+ok 14 - use the same checkout for Git and CVS
+# passed all 14 test(s)
+1..14
