b0ecd38dc333bc5638e35941c90da2ce1d48ece0
smolder on v.nix.is <smolder@v.nix.is>
2011-03-21 06:01:02 +0000

    Bumping smolder reports for Git

---
+++ 705/t6004-rev-list-path-optim.sh
@@ -0,0 +1,9 @@
+ok 1 - setup
+ok 2 - path-optimization
+ok 3 - further setup
+ok 4 - path optimization 2
+ok 5 - pathspec with leading path
+ok 6 - pathspec with glob (1)
+ok 7 - pathspec with glob (2)
+# passed all 7 test(s)
+1..7
