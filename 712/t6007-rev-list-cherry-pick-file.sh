0c0f1fcf7e0fd391e00d54e1ca015920cb92febe
smolder on v.nix.is <smolder@v.nix.is>
2011-03-23 06:01:57 +0000

    Bumping smolder reports for Git

---
+++ 712/t6007-rev-list-cherry-pick-file.sh
@@ -0,0 +1,16 @@
+ok 1 - setup
+ok 2 - --left-right
+ok 3 - --count
+ok 4 - --cherry-pick foo comes up empty
+ok 5 - --cherry-pick bar does not come up empty
+ok 6 - bar does not come up empty
+ok 7 - --cherry-pick bar does not come up empty (II)
+ok 8 - --cherry-mark
+ok 9 - --cherry-mark --left-right
+ok 10 - --cherry-pick --right-only
+ok 11 - --cherry-pick --left-only
+ok 12 - --cherry
+ok 13 - --cherry-pick with independent, but identical branches
+ok 14 - --count --left-right
+# passed all 14 test(s)
+1..14
