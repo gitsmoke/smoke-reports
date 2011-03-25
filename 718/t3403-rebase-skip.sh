09ac787b5eef9792647a999fa0f9a0773255197d
smolder on v.nix.is <smolder@v.nix.is>
2011-03-25 06:01:51 +0000

    Bumping smolder reports for Git

---
+++ 718/t3403-rebase-skip.sh
@@ -0,0 +1,12 @@
+ok 1 - setup
+ok 2 - rebase with git am -3 (default)
+ok 3 - rebase --skip can not be used with other options
+ok 4 - rebase --skip with am -3
+ok 5 - rebase moves back to skip-reference
+ok 6 - checkout skip-merge
+ok 7 - rebase with --merge
+ok 8 - rebase --skip with --merge
+ok 9 - merge and reference trees equal
+ok 10 - moved back to branch correctly
+# passed all 10 test(s)
+1..10
---
+++ 719/t3030-merge-recursive.sh
@@ -0,0 +1,34 @@
+ok 1 - setup 1
+ok 2 - setup 2
+ok 3 - setup 3
+ok 4 - setup 4
+ok 5 - setup 5
+ok 6 - setup 6
+ok 7 - setup 7
+ok 8 - setup 8
+ok 9 - setup 9
+ok 10 - merge-recursive simple
+ok 11 - merge-recursive result
+ok 12 - fail if the index has unresolved entries
+ok 13 - merge-recursive remove conflict
+ok 14 - merge-recursive remove conflict
+ok 15 - merge-recursive d/f simple
+ok 16 - merge-recursive result
+ok 17 - merge-recursive d/f conflict
+ok 18 - merge-recursive d/f conflict result
+ok 19 - merge-recursive d/f conflict the other way
+ok 20 - merge-recursive d/f conflict result the other way
+ok 21 - merge-recursive d/f conflict
+ok 22 - merge-recursive d/f conflict result
+ok 23 - merge-recursive d/f conflict
+ok 24 - merge-recursive d/f conflict result
+ok 25 - reset and 3-way merge
+ok 26 - reset and bind merge
+ok 27 - merge removes empty directories
+not ok 28 - merge-recursive simple w/submodule # TODO known breakage
+not ok 29 - merge-recursive simple w/submodule result # TODO known breakage
+ok 30 - merge-recursive copy vs. rename
+ok 31 - merge-recursive rename vs. rename/symlink
+# still have 2 known breakage(s)
+# passed all remaining 29 test(s)
+1..31
