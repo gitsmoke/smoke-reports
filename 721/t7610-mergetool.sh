5943552183a101c0207fea9d78ffbc4369c0de51
smolder on v.nix.is <smolder@v.nix.is>
2011-03-26 06:01:12 +0000

    Bumping smolder reports for Git

---
+++ 721/t7610-mergetool.sh
@@ -0,0 +1,10 @@
+ok 1 - setup
+ok 2 - custom mergetool
+ok 3 - mergetool crlf
+ok 4 - mergetool in subdir
+ok 5 - mergetool on file in parent dir
+ok 6 - mergetool skips autoresolved
+ok 7 - mergetool merges all from subdir
+ok 8 - mergetool skips resolved paths when rerere is active
+# passed all 8 test(s)
+1..8
