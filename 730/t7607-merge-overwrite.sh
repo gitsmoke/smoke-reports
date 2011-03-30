fbf3d33a679995b3b1fedb92696136a980def7e7
smolder on v.nix.is <smolder@v.nix.is>
2011-03-30 20:00:51 +0000

    Bumping smolder reports for Git

---
+++ 730/t7607-merge-overwrite.sh
@@ -0,0 +1,17 @@
+ok 1 - setup
+ok 2 - will not overwrite untracked file
+ok 3 - will overwrite tracked file
+ok 4 - will not overwrite new file
+ok 5 - will not overwrite staged changes
+ok 6 - will not overwrite removed file
+ok 7 - will not overwrite re-added file
+ok 8 - will not overwrite removed file with staged changes
+ok 9 - will not overwrite untracked subtree
+ok 10 - will not overwrite untracked file in leading path
+ok 11 - will not overwrite untracked symlink in leading path
+ok 12 - will not be confused by symlink in leading path
+ok 13 - will not overwrite untracked file on unborn branch
+ok 14 - set up unborn branch and content
+ok 15 - will not clobber WT/index when merging into unborn
+# passed all 15 test(s)
+1..15
