7a1cdb3afb4b95d2ec26c4c3ca21dbdea1e29086
smolder on v.nix.is <smolder@v.nix.is>
2011-04-02 04:00:45 +0000

    Bumping smolder reports for Git

---
+++ 736/t7406-submodule-update.sh
@@ -0,0 +1,18 @@
+ok 1 - setup a submodule tree
+ok 2 - submodule update detaching the HEAD 
+ok 3 - submodule update does not fetch already present commits
+ok 4 - submodule update --rebase staying on master
+ok 5 - submodule update --merge staying on master
+ok 6 - submodule update - rebase in .git/config
+ok 7 - submodule update - checkout in .git/config but --rebase given
+ok 8 - submodule update - merge in .git/config
+ok 9 - submodule update - checkout in .git/config but --merge given
+ok 10 - submodule update - checkout in .git/config
+ok 11 - submodule init picks up rebase
+ok 12 - submodule init picks up merge
+ok 13 - submodule update --merge  - ignores --merge  for new submodules
+ok 14 - submodule update --rebase - ignores --rebase for new submodules
+ok 15 - submodule update ignores update=merge config for new submodules
+ok 16 - submodule update ignores update=rebase config for new submodules
+# passed all 16 test(s)
+1..16
