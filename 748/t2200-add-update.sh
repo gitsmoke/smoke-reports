8a6f5056a899aac5956f5245ddfa5303d5159095
smolder on v.nix.is <smolder@v.nix.is>
2011-04-08 04:00:59 +0000

    Bumping smolder reports for Git

---
+++ 748/t2200-add-update.sh
@@ -0,0 +1,22 @@
+ok 1 - setup
+ok 2 - update
+ok 3 - update noticed a removal
+ok 4 - update touched correct path
+ok 5 - update did not touch other tracked files
+ok 6 - update did not touch untracked files
+ok 7 - cache tree has not been corrupted
+ok 8 - update from a subdirectory
+ok 9 - change gets noticed
+ok 10 - update from a subdirectory without pathspec
+ok 11 - update from a subdirectory with local pathspec
+ok 12 - update from a subdirectory with magic pathspec (mnemonic)
+ok 13 - update from a subdirectory with magic pathspec (longhand)
+ok 14 - replace a file with a symlink
+ok 15 - add everything changed
+ok 16 - touch and then add -u
+ok 17 - touch and then add explicitly
+ok 18 - add -n -u should not add but just report
+ok 19 - add -u resolves unmerged paths
+ok 20 - "add -u non-existent" should fail
+# passed all 20 test(s)
+1..20
