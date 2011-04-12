db2deb4b7ac6d1dee02913a1bc225c46f289e355
smolder on v.nix.is <smolder@v.nix.is>
2011-04-12 04:00:52 +0000

    Bumping smolder reports for Git

---
+++ 755/t9111-git-svn-use-svnsync-props.sh
@@ -0,0 +1,6 @@
+ok 1 - load svnsync repo
+ok 2 - verify metadata for /bar
+ok 3 - verify metadata for /dir/a/b/c/d/e
+ok 4 - verify metadata for /dir
+# passed all 4 test(s)
+1..4
---
+++ 755/t9158-git-svn-mergeinfo.sh
@@ -0,0 +1,7 @@
+define NO_SVN_TESTS to skip git svn tests
+ok 1 - initialize source svn repo
+ok 2 - clone svn repo
+ok 3 - change svn:mergeinfo
+ok 4 - verify svn:mergeinfo
+# passed all 4 test(s)
+1..4
