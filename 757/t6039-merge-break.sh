e725ca2000ab9eac4f20064fb9f89570a9c0eb56
smolder on v.nix.is <smolder@v.nix.is>
2011-04-13 04:00:36 +0000

    Bumping smolder reports for Git

---
+++ 757/t6039-merge-break.sh
@@ -0,0 +1,11 @@
+ok 1 - setup baseline
+ok 2 - merge rename to tweak finds rename
+ok 3 - merge tweak to rename finds rename
+ok 4 - merge rename to rename/tweak (one replacement)
+ok 5 - merge rename/tweak to rename (one replacement)
+ok 6 - merge rename to rename/tweak (two replacements, same)
+ok 7 - merge rename/tweak to rename (two replacements, same)
+ok 8 - merge rename to rename/tweak (two replacements, diff)
+ok 9 - merge rename to rename/tweak (two replacements, diff)
+# passed all 9 test(s)
+1..9
---
+++ 757/t9105-git-svn-commit-diff.sh
@@ -0,0 +1,5 @@
+ok 1 - initialize repo
+ok 2 - test the commit-diff command
+ok 3 - commit-diff to a sub-directory (with git svn config)
+# passed all 3 test(s)
+1..3
