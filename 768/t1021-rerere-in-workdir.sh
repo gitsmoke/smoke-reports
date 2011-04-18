670011f2e4e102228824573f4ac864ec0d5f9faa
smolder on v.nix.is <smolder@v.nix.is>
2011-04-18 04:00:53 +0000

    Bumping smolder reports for Git

---
+++ 768/t1021-rerere-in-workdir.sh
@@ -0,0 +1,6 @@
+ok 1 - setup
+ok 2 - rerere in workdir
+not ok 3 - rerere in workdir (relative) # TODO known breakage
+# still have 1 known breakage(s)
+# passed all remaining 2 test(s)
+1..3
---
+++ 769/t9131-git-svn-empty-symlink.sh
@@ -0,0 +1,17 @@
+ok 1 - load svn dumpfile
+ok 2 - clone using git svn
+ok 3 - enable broken symlink workaround
+ok 4 - "bar" is an empty file
+ok 5 - get "bar" => symlink fix from svn
+ok 6 - "bar" becomes a symlink
+ok 7 - clone using git svn
+ok 8 - disable broken symlink workaround
+ok 9 - "bar" is an empty file
+ok 10 - get "bar" => symlink fix from svn
+ok 11 - "bar" does not become a symlink
+ok 12 - clone using git svn
+ok 13 - "bar" is an empty file
+ok 14 - get "bar" => symlink fix from svn
+ok 15 - "bar" does not become a symlink
+# passed all 15 test(s)
+1..15
