84c8749ee8b2fc998871a3c2e74a16552b019dce
smolder on v.nix.is <smolder@v.nix.is>
2011-04-25 04:01:40 +0000

    Bumping smolder reports for Git

---
+++ 783/t0026-eol-config.sh
@@ -0,0 +1,7 @@
+ok 1 - setup
+ok 2 - eol=lf puts LFs in normalized file
+ok 3 - eol=crlf puts CRLFs in normalized file
+ok 4 - autocrlf=true overrides eol=lf
+ok 5 - autocrlf=true overrides unset eol
+# passed all 5 test(s)
+1..5
---
+++ 783/t4133-apply-filenames.sh
@@ -0,0 +1,4 @@
+ok 1 - setup
+ok 2 - apply diff with inconsistent filenames in headers
+# passed all 2 test(s)
+1..2
