fc4fb3b379d7a2c041ba7e70002a5f7b05e6ff80
smolder on v.nix.is <smolder@v.nix.is>
2011-05-05 04:00:51 +0000

    Bumping smolder reports for Git

---
+++ 805/t0061-run-command.sh
@@ -0,0 +1,5 @@
+ok 1 - start_command reports ENOENT
+ok 2 - run_command can run a command
+ok 3 - run_command reports EACCES
+# passed all 3 test(s)
+1..3
---
+++ 805/t9145-git-svn-master-branch.sh
@@ -0,0 +1,4 @@
+ok 1 - setup test repository
+ok 2 - git svn clone --stdlayout sets up trunk as master
+# passed all 2 test(s)
+1..2
