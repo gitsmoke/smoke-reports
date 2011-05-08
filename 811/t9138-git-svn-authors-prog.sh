fcd832bf205bf07030a674ec8d241037cbe1d052
smolder on v.nix.is <smolder@v.nix.is>
2011-05-08 04:01:53 +0000

    Bumping smolder reports for Git

---
+++ 811/t9138-git-svn-authors-prog.sh
@@ -0,0 +1,8 @@
+ok 1 - setup svnrepo
+ok 2 - import authors with prog and file
+ok 3 - imported 6 revisions successfully
+ok 4 - authors-prog ran correctly
+ok 5 - authors-file overrode authors-prog
+ok 6 - authors-prog handled special characters in username
+# passed all 6 test(s)
+1..6
