8b4556fce8407ca95be451e63646e7394622a8ca
smolder on v.nix.is <smolder@v.nix.is>
2011-05-10 04:02:19 +0000

    Bumping smolder reports for Git

---
+++ 815/t4034-diff-words.sh
@@ -0,0 +1,35 @@
+ok 1 - setup
+ok 2 - set up pre and post with runs of whitespace
+ok 3 - word diff with runs of whitespace
+ok 4 - --word-diff=porcelain
+ok 5 - --word-diff=plain
+ok 6 - --word-diff=plain --color
+ok 7 - word diff without context
+ok 8 - word diff with a regular expression
+ok 9 - set up a diff driver
+ok 10 - option overrides .gitattributes
+ok 11 - use regex supplied by driver
+ok 12 - set up diff.wordRegex option
+ok 13 - command-line overrides config
+ok 14 - command-line overrides config: --word-diff-regex
+ok 15 - .gitattributes override config
+ok 16 - setup: remove diff driver regex
+ok 17 - use configured regex
+ok 18 - test parsing words for newline
+ok 19 - test when words are only removed at the end
+ok 20 - --word-diff=none
+ok 21 - diff driver 'bibtex'
+ok 22 - diff driver 'cpp'
+ok 23 - diff driver 'csharp'
+ok 24 - diff driver 'fortran'
+ok 25 - diff driver 'html'
+ok 26 - diff driver 'java'
+ok 27 - diff driver 'objc'
+ok 28 - diff driver 'pascal'
+ok 29 - diff driver 'perl'
+ok 30 - diff driver 'php'
+ok 31 - diff driver 'python'
+ok 32 - diff driver 'ruby'
+ok 33 - diff driver 'tex'
+# passed all 33 test(s)
+1..33
---
+++ 816/t7501-commit.sh
@@ -0,0 +1,46 @@
+ok 1 - initial status
+ok 2 - Constructing initial commit
+ok 3 - fail initial amend
+ok 4 - initial commit
+ok 5 - invalid options 1
+ok 6 - invalid options 2
+ok 7 - using paths with -a
+ok 8 - can use paths with --interactive
+ok 9 - using invalid commit with -C
+ok 10 - testing nothing to commit
+ok 11 - next commit
+ok 12 - commit message from non-existing file
+ok 13 - empty commit message
+ok 14 - commit message from file
+ok 15 - amend commit
+ok 16 - passing -m and -F
+ok 17 - using message from other commit
+ok 18 - editing message from other commit
+ok 19 - message from stdin
+ok 20 - overriding author from command line
+ok 21 - commit --author output mentions author
+ok 22 - interactive add
+ok 23 - showing committed revisions
+ok 24 - editor not invoked if -F is given
+ok 25 - validate git rev-list output.
+ok 26 - partial commit that involves removal (1)
+ok 27 - partial commit that involves removal (2)
+ok 28 - partial commit that involves removal (3)
+ok 29 - amend commit to fix author
+ok 30 - amend commit to fix date
+ok 31 - commit complains about bogus date
+ok 32 - sign off (1)
+ok 33 - sign off (2)
+ok 34 - signoff gap
+ok 35 - signoff gap 2
+ok 36 - multiple -m
+ok 37 - amend commit to fix author
+ok 38 - git commit <file> with dirty index
+ok 39 - same tree (single parent)
+ok 40 - same tree (single parent) --allow-empty
+ok 41 - same tree (merge and amend merge)
+ok 42 - amend using the message from another commit
+ok 43 - amend using the message from a commit named with tag
+ok 44 - amend can copy notes
+# passed all 44 test(s)
+1..44
