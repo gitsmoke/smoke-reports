2d2ed8911d3fbede5ae9d9a3a86d10e3bf95d5eb
smolder on v.nix.is <smolder@v.nix.is>
2011-05-11 04:01:15 +0000

    Bumping smolder reports for Git

---
+++ 818/t3703-add-magic-pathspec.sh
@@ -0,0 +1,8 @@
+ok 1 - setup
+ok 2 - add :/
+ok 3 - add :/anothersub
+ok 4 - add :/non-existent
+ok 5 - a file with the same (long) magic name exists
+ok 6 - a file with the same (short) magic name exists
+# passed all 6 test(s)
+1..6
