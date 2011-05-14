5b7f43337fe3b56084a8f44242611cb1ae78c734
smolder on v.nix.is <smolder@v.nix.is>
2011-05-14 04:01:23 +0000

    Bumping smolder reports for Git

---
+++ 824/t1020-subdirectory.sh
@@ -0,0 +1,15 @@
+ok 1 - setup
+ok 2 - update-index and ls-files
+ok 3 - cat-file
+ok 4 - diff-files
+ok 5 - write-tree
+ok 6 - checkout-index
+ok 7 - read-tree
+ok 8 - alias expansion
+ok 9 - !alias expansion
+ok 10 - GIT_PREFIX for !alias
+ok 11 - no file/rev ambiguity check inside .git
+ok 12 - no file/rev ambiguity check inside a bare repo
+ok 13 - detection should not be fooled by a symlink
+# passed all 13 test(s)
+1..13
