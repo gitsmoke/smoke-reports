94be89e7750782a5631e7c107beded7ec4f82956
smolder on v.nix.is <smolder@v.nix.is>
2011-05-15 04:02:05 +0000

    Bumping smolder reports for Git

---
+++ 826/t9109-git-svn-multi-glob.sh
@@ -0,0 +1,6 @@
+ok 1 - test refspec globbing
+ok 2 - test left-hand-side only globbing
+ok 3 - test another branch
+ok 4 - test disallow multiple globs
+# passed all 4 test(s)
+1..4
---
+++ 827/t1050-large.sh
@@ -0,0 +1,4 @@
+ok 1 - setup
+ok 2 - add a large file
+# passed all 2 test(s)
+1..2
