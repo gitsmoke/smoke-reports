8a63d451e2dbe437ef9995c7c1dae8b736b1b396
smolder on v.nix.is <smolder@v.nix.is>
2011-06-10 04:01:22 +0000

    Bumping smolder reports for Git

---
+++ 882/t5521-pull-options.sh
@@ -0,0 +1,10 @@
+ok 1 - setup
+ok 2 - git pull -q
+ok 3 - git pull
+ok 4 - git pull -v
+ok 5 - git pull -v -q
+ok 6 - git pull -q -v
+ok 7 - git pull --force
+ok 8 - git pull --all
+# passed all 8 test(s)
+1..8
---
+++ 883/t9118-git-svn-funky-branch-names.sh
@@ -0,0 +1,7 @@
+ok 1 - setup svnrepo
+ok 2 - test clone with funky branch names
+ok 3 - test dcommit to funky branch
+ok 4 - test dcommit to scary branch
+ok 5 - test dcommit to trailing_dotlock branch
+# passed all 5 test(s)
+1..5
