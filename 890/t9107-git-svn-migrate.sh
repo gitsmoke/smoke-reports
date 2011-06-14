5b33e3d1ea9c71c3b1051503c32c4912905fb076
smolder on v.nix.is <smolder@v.nix.is>
2011-06-14 04:01:13 +0000

    Bumping smolder reports for Git

---
+++ 890/t9107-git-svn-migrate.sh
@@ -0,0 +1,9 @@
+ok 1 - setup old-looking metadata
+ok 2 - git-svn-HEAD is a real HEAD
+ok 3 - initialize old-style (v0) git svn layout
+ok 4 - initialize a multi-repository repo
+ok 5 - multi-fetch works on partial urls + paths
+ok 6 - migrate --minimize on old inited layout
+ok 7 - .rev_db auto-converted to .rev_map.UUID
+# passed all 7 test(s)
+1..7
