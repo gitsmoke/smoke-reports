af39ca4c2e16cbee5b3a725836c793ea3a40ef81
smolder on v.nix.is <smolder@v.nix.is>
2011-06-20 04:01:19 +0000

    Bumping smolder reports for Git

---
+++ 902/t4046-diff-unmerged.sh
@@ -0,0 +1,7 @@
+ok 1 - setup
+ok 2 - diff-files -0
+ok 3 - diff-files -1
+ok 4 - diff-files -2
+ok 5 - diff-files -3
+# passed all 5 test(s)
+1..5
---
+++ 902/t9144-git-svn-old-rev_map.sh
@@ -0,0 +1,4 @@
+ok 1 - setup test repository with old layout
+ok 2 - old layout continues to work
+# passed all 2 test(s)
+1..2
