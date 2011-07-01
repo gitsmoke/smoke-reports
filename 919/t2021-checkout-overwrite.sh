47fd913f4d2cdf549968bebcdefab30ea89be379
smolder on v.nix.is <smolder@v.nix.is>
2011-07-01 04:02:36 +0000

    Bumping smolder reports for Git

---
+++ 919/t2021-checkout-overwrite.sh
@@ -0,0 +1,7 @@
+ok 1 - setup
+ok 2 - create a commit where dir a/b changed to file
+ok 3 - checkout commit with dir must not remove untracked a/b
+ok 4 - create a commit where dir a/b changed to symlink
+ok 5 - checkout commit with dir must not remove untracked a/b
+# passed all 5 test(s)
+1..5
